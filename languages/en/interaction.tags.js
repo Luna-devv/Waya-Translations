614d1ec38b854bd53d98a9ecc54eafe660388e71
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-05-06 19:10:33 +0200

    tags interaction & modals

---
+++ languages/en/interaction.tags.js
@@ -0,0 +1,18 @@
+module.exports = {
+    alreadyExists: 'A Tag with this name already exists!',
+    invalidTag: 'A Tag with this name doesn\'t exist!',
+    form: {
+        formName: '{name} tag',
+        title: 'Title',
+        description: 'Description',
+        thumbnail: 'Thumbnail',
+        image: 'Image',
+        placeholder: 'PLACEHOLDERS (Do Not Edit)',
+        placeholderSecret: 'I said don\'t edit :('
+    },
+    list: {
+        title: 'Tag list',
+        none: 'no tags'
+    },
+    deleted: 'Successfully deleted the Tag **{name}**.'
+};
