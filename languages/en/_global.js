fd64714553c5f854bcf7e13ec7a38503a750b93e
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-05-09 13:06:18 +0200

    global values

---
+++ languages/en/_global.js
@@ -0,0 +1,13 @@
+module.exports = {
+    disabled: 'You have been disabled.',
+    permissions: {
+        user: {
+            guild: 'You cannot run this action since you\'re missing the {perms} permissions.',
+            channel: 'You cannot run this action since you\'re missing the {perms} permissions in this channel.'
+        },
+        me: {
+            guild: 'I cannot run this action since I\'m missing the {perms} permissions.',
+            channel: 'I cannot run this action since I\'m missing the {perms} permissions in this channel.'
+        }
+    }
+};
