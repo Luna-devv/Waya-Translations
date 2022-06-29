ef01236fda0248603498b275db48590b25012906
Lunish <luna@waya.one>
2022-06-29 23:33:12 +0900

    new tag creation UI

--- languages/en/interaction.tags.js
+++ languages/en/interaction.tags.js
@@ -2,14 +2,32 @@ module.exports = {
     alreadyExists: 'A Tag with this name already exists!',
     alreadyExistsDefault: 'A default Command with this name already exists!',
     invalidTag: 'A Tag with this name doesn\'t exist!',
+    overview: {
+        title: 'Tag Config',
+        message: 'Message',
+        embed: 'Embed',
+        placeholder: 'Useable Placeholders',
+        button: {
+            exit: 'Save & Exit',
+            permission: 'Select a permission the user needs'
+        }
+    },
     form: {
         formName: '{name} tag',
-        title: 'Title',
-        description: 'Description',
-        thumbnail: 'Thumbnail',
-        image: 'Image',
-        placeholder: 'PLACEHOLDERS (Do Not Edit)',
-        placeholderSecret: 'I said don\'t edit :('
+        message: {
+            content: 'Content'
+        },
+        embed: {
+            title: 'Title',
+            description: 'Description',
+            thumbnail: 'Thumbnail',
+            image: 'Image',
+            footer: 'Footer'
+        }
+    },
+    success: {
+        message: 'Successfully added the Tag {name}.',
+        command: 'Try it out with {command}'
     },
     list: {
         title: 'Tag list',
