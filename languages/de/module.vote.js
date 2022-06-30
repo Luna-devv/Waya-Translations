be4ca634f2919b796eff2573ef028e0019ac3c5d
Lunish <luni.siaa@gmail.com>
2022-07-01 00:49:20 +0900

    New translations module.vote.js (German)

--- languages/de/module.vote.js
+++ languages/de/module.vote.js
@@ -1,9 +1,9 @@
 module.exports = {
-    greeting: 'Hallo, {user}',
+    greeting: 'Hello there, {user}',
     description: {
         remind: 'Du kannst nun wieder für mich auf top.gg Voten!',
-        data: 'Du hast das letze mal {relativeTime}** und insgesammt **{amount}** mal gevotet.',
-        new: '**NEU!** Unterstüze uns auch auf {url} mit deinem Vote!'
+        data: 'Du hast das letzte Mal {relativeTime}** und insgesamt **{amount}** Mal gevotet.',
+        new: '**NEU!** Unterstütze uns auch auf {url} mit deinem Vote!'
     },
     button: { vote: 'Vote' }
 };
