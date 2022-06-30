228fba20dbeb3172a4785dae556c6223095efa6c
Lunish <luni.siaa@gmail.com>
2022-07-01 00:43:38 +0900

    New translations context.ForceStarboard.js (German)

--- languages/de/context.ForceStarboard.js
+++ languages/de/context.ForceStarboard.js
@@ -4,9 +4,9 @@ module.exports = {
         content: 'Die Nachricht hat keinen Inhalt oder Anhänge..',
         channel: 'Der eingerichtete Starboard Channel exestiert nicht mehr..',
         exists: {
-            text: 'Die Nachricht ist bereits im Starboard!',
+            text: 'Die Nachricht ist bereits im Starboard..',
             button: 'Zur Nachricht'
         }
     },
-    success: 'Die nachricht ist num im starboard!'
+    success: 'The message is now in the Starboard.'
 };
