74b971c2aa80ebe3d4034cda2279e012a0ca6cb4
Lunish <luni.siaa@gmail.com>
2022-07-01 00:49:37 +0900

    New translations interaction.help.js (Croatian)

--- languages/hr/interaction.help.js
+++ languages/hr/interaction.help.js
@@ -1,8 +1,8 @@
 module.exports = {
-    description: '**O {name}**\nNapravila me je {devs} ikone od [Danu](https://waya.one/go/danu_icons).\n{name} je napravljena da bude vrlo jednostavna u procesu konfiguracije, ali u isto vrijeme pruža najbolju prilagodljivost. Sve sve može učiniti pomoću jednostavne \'/config\' komande bez ikakvih premium uvjeta.\n\n**Dobivanje pomoći**\nAko postane naporno za vas jednostavno uđite u [Pomoćni Server]({support}) i pitajte zajednicu za pomoć.',
-    footer: 'Online na {time} • Statistika je premještena u \'/stats\'',
+    description: '**O {name}**\nRazvijena sam od {devs} ikone od [Danu](https://waya.one/go/danu_icons).\n{name} je napravljena da bude vrlo jednostavna u svom procesu konfiguracije, ali u isto vrijeme pruža najbolju prilagodljivost. Sve se može učiniti u jednoj jednostavnoj naredbi `/config` bez ikakvih premium zahtjeva.\n\n**Dobivanje pomoći**\nAko vam postane preteško, jednostavno se pridružite [Pomoćnom Serveru]({support}) i tamo potajte zajednicu za pomoć.',
+    footer: 'Online za {time} • Statistika je premještena u \'/stats\'',
     button: {
-        learnmore: 'Saznajte više',
-        vote: 'Glasujte'
+        learnmore: 'Saznajte Više',
+        vote: 'Glasajte'
     }
 };
