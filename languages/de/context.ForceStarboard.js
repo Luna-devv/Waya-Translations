06bdd1e7ce233efdf591936f5885304d5bf0d4a7
Lunish <luni.siaa@gmail.com>
2022-07-01 05:07:16 +0900

    New translations context.ForceStarboard.js (German)

--- languages/de/context.ForceStarboard.js
+++ languages/de/context.ForceStarboard.js
@@ -1,7 +1,7 @@
 module.exports = {
     fail: {
         config: 'Das Starboard ist in diesem Server nicht eingerichtet..',
-        content: 'Message has no content nor attachments..',
+        content: 'Message has no content not attachments..',
         channel: 'Der eingerichtete Starboard Channel exestiert nicht mehr..',
         exists: {
             text: 'Die Nachricht ist bereits im Starboard..',
