1a4704ee9ec5626bef46c4ce6e53dc0351e16db7
Lunish <luni.siaa@gmail.com>
2022-06-30 20:25:51 +0900

    New translations _global.js (French)

---
+++ languages/fr/_global.js
@@ -0,0 +1,41 @@
+module.exports = {
+    dms: 'This command cannot be used inside of private messages.',
+    disabled: 'You have been disabled.',
+    error: 'An error occurred while executing this command.\nPlease join the support server and report this issue\nwith the id {issue}!',
+    premium: 'premium required',
+    button: { add: 'Add {name}' },
+    permissions: {
+        user: {
+            guild: 'You cannot run this action since you\'re missing the {perms} permissions.',
+            channel: 'You cannot run this action since you\'re missing the {perms} permissions in this channel.',
+            roles: {
+                user: {
+                    text: 'You cannot perform this action since the highest role of that user is higher than yours.',
+                    user: 'User: {u.role} @ index {u.index}',
+                    you: 'You: {y.role} @ index {y.index}'
+                },
+                role: {
+                    text: 'You cannot perform this action since your highest role is lower than the role to manage.',
+                    user: 'Role: {u.role} @ index {u.index}',
+                    you: 'You: {y.role} @ index {y.index}'
+                }
+            }
+        },
+        me: {
+            guild: 'I cannot run this action since I\'m missing the {perms} permissions.',
+            channel: 'I cannot run this action since I\'m missing the {perms} permissions in this channel.',
+            roles: {
+                user: {
+                    text: 'I cannot perform this action since the highest role of that user is higher than mine.',
+                    user: 'User: {u.role} @ index {u.index}',
+                    bot: 'Me: {b.role} @ index {b.index}'
+                },
+                role: {
+                    text: 'I cannot perform this action since my highest role is higher than the role to manage.',
+                    user: 'Role: {u.role} @ index {u.index}',
+                    bot: 'Me: {b.role} @ index {b.index}'
+                }
+            }
+        }
+    }
+};
