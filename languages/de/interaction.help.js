61bc2e92a5bb7a5ea81bc74eadd05a255a2795e9
Lunish <luni.siaa@gmail.com>
2022-07-01 00:45:09 +0900

    New translations interaction.help.js (German)

--- languages/de/interaction.help.js
+++ languages/de/interaction.help.js
@@ -1,8 +1,8 @@
 module.exports = {
-    description: '**Über {name}**\nIch wurde von {devs} entwickelt. Die icons sind von [Danu](https://waya.one/go/danu_icons).\n{name} ist gemacht um im Configurations Prozess sehr einfach zu sein, aber zugleich auch die beste Personalisierbarkeit bereit zu stellen. Alles kann in einem einfachen `/config` Command ohne jegliche Einschränkungen eingerichtet werden.\n\n**Hilfe bekommen**\nWenn es dir etwas zu viel wird, trete einfach dem [Support Server]({support}) bei und frage die Community dort für Hilfe.',
+    description: '**About {name}**\nI\'m developed by {devs} icons by [Danu](https://waya.one/go/danu_icons).\n{name} is made to be very simple in it\'s configuration process but at the same time there to provide the best customizability. Everything can be done in one simple `/config` command without any premium requirements.\n\n**Getting help**\nIf it gets to overwhelming for you simply join the [Support Server]({support}) and ask the community for help there.',
     footer: 'Online für {time} • Stats sind nun in \'/stats\'',
     button: {
         learnmore: 'Learn More',
-        vote: 'Voten'
+        vote: 'Vote'
     }
 };
