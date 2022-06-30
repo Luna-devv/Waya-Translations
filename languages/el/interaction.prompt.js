6d39e66dbffe512ace98d1fb8838180ffcf30c32
Lunish <luni.siaa@gmail.com>
2022-06-30 20:32:04 +0900

    New translations interaction.prompt.js (Greek)

---
+++ languages/el/interaction.prompt.js
@@ -0,0 +1,21 @@
+module.exports = {
+    validate: 'Validate data...',
+    create: 'Creating {type} prompt...',
+    tooManyRoles: 'You have {roles} roles in your server, please make sure that you have less than 244!',
+    created: 'The {type} prompt has been created.',
+    type: {
+        category: 'channel category',
+        support: 'support role',
+        emote: 'custom emote'
+    },
+    message: {
+        created: 'created',
+        noSupport: 'none (use **/config**)',
+        invalidEmote: 'not in same guild',
+        content: 'Content'
+    },
+    error: {
+        tooLong: 'Option {index} has more than {number} characters!',
+        noData: 'no data set'
+    }
+};
