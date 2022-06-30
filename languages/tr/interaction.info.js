f93e2e1f13149f6c7c9de1d9afd14f770b490df4
Lunish <luni.siaa@gmail.com>
2022-06-30 20:31:08 +0900

    New translations interaction.info.js (Turkish)

---
+++ languages/tr/interaction.info.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: '{user}\'s Invites',
+    building: 'Building...',
+    more: '+ {count} more',
+    none: 'none',
+    button: {
+        ids: 'User IDs',
+        mentions: 'User Mentions'
+    }
+};
