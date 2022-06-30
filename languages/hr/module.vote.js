57b907a9c7397de8b23c105db3dc06459a9e7325
Lunish <luni.siaa@gmail.com>
2022-07-01 00:49:43 +0900

    New translations module.vote.js (Croatian)

---
+++ languages/hr/module.vote.js
@@ -0,0 +1,9 @@
+module.exports = {
+    greeting: 'Pozdrav, {user}',
+    description: {
+        remind: 'Sada možete ponovno glasati za mene na top.gg!',
+        data: '**Posljednji put ste glasali {relativeTime}** i ukupno {amount} puta.',
+        new: '**NOVO!** Također nas podržite na {url} svojim glasom!'
+    },
+    button: { vote: 'Glasujte' }
+};
