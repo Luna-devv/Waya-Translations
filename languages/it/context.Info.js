df300f3da80be8a542a121d352a02fa69de1539f
Lunish <luna@waya.one>
2022-07-01 01:27:22 +0900

    Update context.Info.js

--- languages/it/context.Info.js
+++ languages/it/context.Info.js
@@ -1,35 +1,12 @@
 module.exports = {
-    messages: 'Messages',
-    name: 'Name',
+    messages: 'messaggi',
+    name: 'Nome',
     id: 'Id',
-    created: 'Created',
-    joined: 'Joined',
-    invited: 'Invited by',
-    none: 'none',
+    created: `creato`,
+    joined: 'unito',
+    invited: 'invitato ',
     pronouns: {
-        title: 'Pronouns',
-        none: 'No pronouns set'
-    },
-    actions: {
-        notAllowed: 'You can\'t use this menu.',
-        invalidTime: 'The duration cannot be less than a second!',
-        otherInvalidTime: 'The duration cannot be more than 28 days!',
-        placeholder: 'Choose an action',
-        option: {
-            ban: 'Ban User',
-            kick: 'Kick User',
-            timeout: 'Timeout User',
-            role: 'Assign Role'
-        },
-        form: {
-            reason: 'Reason',
-            note: 'Moderator Note',
-            duration: 'Duration'
-        },
-        success: {
-            ban: 'Successfully banned {user} from {guild}.',
-            kick: 'Successfully kicked {user} from {guild}.',
-            timeout: 'Successfully timed {user} out in {guild}.'
-        }
+        title: `Pronomi`,
+        none: `Non sono stati dati pronomi`
     }
 };
