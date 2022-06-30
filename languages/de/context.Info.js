3bad2e9fb2ed402202d97bc90da1ae75ffbbf6ad
Lunish <luni.siaa@gmail.com>
2022-06-30 20:29:17 +0900

    New translations context.Info.js (German)

--- languages/de/context.Info.js
+++ languages/de/context.Info.js
@@ -2,26 +2,34 @@ module.exports = {
     messages: 'Nachrichten',
     name: 'Name',
     id: 'Id',
-    created: `Erstellt`,
+    created: 'Created',
     joined: 'Beigetreten',
     invited: 'Eingeladen',
+    none: 'none',
     pronouns: {
-        title: `Pronomen`,
-        none: `Keine Pronomen angegeben`
+        title: 'Pronouns',
+        none: 'No pronouns set'
     },
     actions: {
         notAllowed: 'Du kannst dieses Menü nicht benutzten.',
+        invalidTime: 'The duration cannot be less than a second!',
+        otherInvalidTime: 'The duration cannot be more than 28 days!',
         placeholder: 'Wähle eine Aktion',
         option: {
             ban: 'User Bannen',
             kick: 'User Kicken',
-            timeout: 'User Muten'
+            timeout: 'User Muten',
+            role: 'Assign Role'
         },
         form: {
-            reason: 'Grund'
+            reason: 'Grund',
+            note: 'Moderator Note',
+            duration: 'Duration'
         },
         success: {
-            ban: '{user} wurde erfolgreich von {guild} gebannt.'
+            ban: '{user} wurde erfolgreich von {guild} gebannt.',
+            kick: 'Successfully kicked {user} from {guild}.',
+            timeout: 'Successfully timed {user} out in {guild}.'
         }
     }
 };
