717c617ff7e5949cafe93050de2327a5bc603e4b
Lunish <luni.siaa@gmail.com>
2022-06-30 20:34:24 +0900

    New translations interaction.stats.js (Ukrainian)

---
+++ languages/uk/interaction.stats.js
@@ -0,0 +1,22 @@
+module.exports = {
+    fetchError: `Could't not fetch data, please try again later!`,
+    links: {
+        website: 'website',
+        invite: 'invite',
+        more: 'in-depth stats'
+    },
+    general: {
+        title: 'General',
+        uptime: '**{time}** uptime',
+        guilds: '**{count}** guilds',
+        users: '**{count}** users',
+        invites: '**{count}** invites',
+        starboards: '**{count}** starboards',
+        executed: '**{count}** executed',
+        usage: '{cpu}% cpu • {mem}% memory'
+    },
+    commandList: {
+        title: 'Popular',
+        none: 'none'
+    }
+};
