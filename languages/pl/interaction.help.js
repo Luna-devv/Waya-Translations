fd92fa0560323fca678570a024b85838c88612d0
Lunish <luni.siaa@gmail.com>
2022-06-30 20:29:36 +0900

    New translations interaction.help.js (Polish)

---
+++ languages/pl/interaction.help.js
@@ -0,0 +1,8 @@
+module.exports = {
+    description: '**About {name}**\nI\'m developed by {devs} icons by [Danu](https://waya.one/go/danu_icons).\n{name} is made to be very simple in it\'s configuration process but at the same time there to provide the best customizability. Everything can be done in one simple `/config` command without any premium requirements.\n\n**Getting help**\nIf it gets to overwhelming for you simply join the [Support Server]({support}) and ask the community for help there.',
+    footer: 'Online for {time} • Stats moved to \'/stats\'',
+    button: {
+        learnmore: 'Learn More',
+        vote: 'Vote'
+    }
+};
