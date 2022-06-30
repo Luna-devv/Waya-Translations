32e7d4eef5c9790f9d25a4251688e6c1a9e97afa
Lunish <luni.siaa@gmail.com>
2022-06-30 20:36:36 +0900

    New translations module.formatTime.js (German)

---
+++ languages/de/module.formatTime.js
@@ -0,0 +1,12 @@
+module.exports = {
+    month: 'month',
+    months: 'months',
+    day: 'day',
+    days: 'days',
+    hour: 'hour',
+    hours: 'hours',
+    minute: 'minute',
+    minutes: 'minutes',
+    second: 'second',
+    seconds: 'seconds'
+};
