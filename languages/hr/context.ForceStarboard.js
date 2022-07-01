ee74e11536ccb7ed76fb57cdc07d502edb24de62
Lunish <luni.siaa@gmail.com>
2022-07-01 20:45:49 +0900

    New translations context.ForceStarboard.js (Croatian)

--- languages/hr/context.ForceStarboard.js
+++ languages/hr/context.ForceStarboard.js
@@ -1,7 +1,7 @@
 module.exports = {
     fail: {
         config: 'Starboard nije postavljen za ovaj server..',
-        content: 'Message has no content not attachments..',
+        content: 'Message has no content or attachments..',
         channel: 'Konfigurirani Starboard kanal više ne postoji..',
         exists: {
             text: 'Poruka je već na Starboard-u..',
