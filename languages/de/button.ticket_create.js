9614a8778a70de21f431474cb0afd1594871a7a3
Lunish <luni.siaa@gmail.com>
2022-08-10 22:50:16 +0200

    New translations button.ticket_create.js (German)

--- languages/de/button.ticket_create.js
+++ languages/de/button.ticket_create.js
@@ -1,10 +1,5 @@
 module.exports = {
     noData: 'Der Server kann nicht in der Datenbank gefunden werden!',
     noCategory: 'The Tickets category has been deleted. Please notify a Server Administrator.',
-    form: {
-        formTitle: 'Ticket opening reason',
-        field: 'Why do you want to open a ticket?',
-        placeholder: 'I need help doing that'
-    },
     created: 'Dein Ticket, {channel}, wurde erstellt!'
 };
