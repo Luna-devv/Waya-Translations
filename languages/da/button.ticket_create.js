bb901573b33cad055545d6faf70bb2274b1d7e4c
Lunish <luni.siaa@gmail.com>
2022-08-10 22:45:13 +0200

    New translations button.ticket_create.js (Danish)

--- languages/da/button.ticket_create.js
+++ languages/da/button.ticket_create.js
@@ -1,5 +1,10 @@
 module.exports = {
     noData: 'Could not find guild in the Database!',
     noCategory: 'The Tickets category has been deleted. Please notify a Server Administrator.',
+    form: {
+        formTitle: 'Ticket opening reason',
+        field: 'Why do you want to open a ticket?',
+        placeholder: 'I need help doing that'
+    },
     created: 'Your ticket {channel} has been created!'
 };
