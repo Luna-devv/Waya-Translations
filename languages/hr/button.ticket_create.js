ddf5a70eed121834888a761d013afe11e1c634d5
Lunish <luni.siaa@gmail.com>
2022-08-11 18:19:18 +0200

    New translations button.ticket_create.js (Croatian)

--- languages/hr/button.ticket_create.js
+++ languages/hr/button.ticket_create.js
@@ -1,5 +1,10 @@
 module.exports = {
     noData: 'Ne mogu pronaći server u bazi podataka!',
     noCategory: 'Kategorija Ulaznica je izbrisana. Molimo obavijestite administratora servera.',
-    created: 'Vaša ulaznica {channel} je stvorena!'
+    created: 'Vaša ulaznica {channel} je stvorena!',
+    form: {
+        formTitle: 'Ticket Opening Reason',
+        field: 'Why do you want to open a Ticket?',
+        placeholder: 'I need help with that..'
+    }
 };
