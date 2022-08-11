3e6a72c73063436b134a9d18f113115f6af7de2b
Lunish <luni.siaa@gmail.com>
2022-08-11 18:19:26 +0200

    New translations interaction.info.js (German)

--- languages/de/interaction.info.js
+++ languages/de/interaction.info.js
@@ -1,39 +1,10 @@
 module.exports = {
-    messages: 'Messages',
-    name: 'Name',
-    id: 'Id',
-    created: 'Created',
-    joined: 'Joined',
-    invited: 'Invited by',
-    none: 'none',
-    pronouns: {
-        title: 'Pronouns',
-        none: 'No pronouns set'
-    },
+    title: '{user}s Einladungen',
+    building: 'Validiere Daten...',
+    more: '+ {count} mehr',
+    none: 'keine',
     button: {
-        avatar: 'Avatar URL',
-        banner: 'Banner URL'
-    },
-    actions: {
-        notAllowed: 'You can\'t use this menu.',
-        invalidTime: 'The duration cannot be less than a second!',
-        otherInvalidTime: 'The duration cannot be more than 28 days!',
-        placeholder: 'Choose an action',
-        option: {
-            ban: 'Ban User',
-            kick: 'Kick User',
-            timeout: 'Timeout User',
-            role: 'Assign Role'
-        },
-        form: {
-            reason: 'Reason',
-            note: 'Moderator Note',
-            duration: 'Duration'
-        },
-        success: {
-            ban: 'Successfully banned {user} from {guild}.',
-            kick: 'Successfully kicked {user} from {guild}.',
-            timeout: 'Successfully timed {user} out in {guild}.'
-        }
+        ids: 'User IDs',
+        mentions: 'User Erwähnungen'
     }
 };
