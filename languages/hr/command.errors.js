5cfd5869298ace2018a9597936bb0ecf56850262
Lunish <luni.siaa@gmail.com>
2022-08-17 00:36:34 +0200

    New translations command.errors.js (Croatian)

---
+++ languages/hr/command.errors.js
@@ -0,0 +1,13 @@
+module.exports = {
+    notFound: 'Ovaj problem nije moguće pronaći',
+    view: {
+        main: 'u {time} tijekom izvršavanja {command} za {user}',
+        fixed: 'Ova greška je ispravljena',
+        unfixed: 'Ova greška nije riješena'
+    },
+    check: {
+        fixed: 'Ažurirano stanje problema u riješeno.',
+        unfixed: 'Stanje problema ažurirano na neriješeno.'
+    },
+    deleted: 'Ovaj problem je izbrisan.'
+};
