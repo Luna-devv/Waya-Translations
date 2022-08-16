943d84f7ab62e7a44cd040df3b52a89600043bab
Lunish <luni.siaa@gmail.com>
2022-08-16 22:26:20 +0200

    Create source file command.votereminder.js

---
+++ languages/en/command.votereminder.js
@@ -0,0 +1,10 @@
+module.exports = {
+    fail: 'You either need to provide `true` or `false` as argument.',
+    success: {
+        disable: 'You\'ve disabled the vote reminder.',
+        enable: 'You\'ve enabled the vote reminder.'
+    },
+    button: {
+        vote: 'Vote on Top.gg'
+    }
+};
