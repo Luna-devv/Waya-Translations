d9ee4f4fbda9c2f133f874b12b2b8146ce5801f7
Lunish <luni.siaa@gmail.com>
2022-08-17 00:35:08 +0200

    New translations command.errors.js (German)

---
+++ languages/de/command.errors.js
@@ -0,0 +1,13 @@
+module.exports = {
+    notFound: 'Dieser Error konnte nicht gefunden werden',
+    view: {
+        main: 'um {time} während der Ausführung von {command} für {user}',
+        fixed: 'Dieser Error wurde behoben',
+        unfixed: 'Dieser Error wurde nicht behoben'
+    },
+    check: {
+        fixed: 'Aktualisierte Error Status zu gelöst.',
+        unfixed: 'Aktualisierte Error Status zu ungelöst.'
+    },
+    deleted: 'Dieser Error wurde nun gelöscht.'
+};
