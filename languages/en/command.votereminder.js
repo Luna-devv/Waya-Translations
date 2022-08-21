16f4d161c741226a078615d3e17270e3b2a761bb
Lunish <luni.siaa@gmail.com>
2022-08-21 22:07:06 +0200

    New translations command.votereminder.js (uwu English)

--- languages/en/command.votereminder.js
+++ languages/en/command.votereminder.js
@@ -1,10 +1,8 @@
 module.exports = {
-    fail: 'You either need to provide `true` or `false` as argument.',
+    fail: 'U eithew nyeed to pwovide `true` ow `false` as awgument.',
     success: {
-        disable: 'You\'ve disabled the vote reminder.',
-        enable: 'You\'ve enabled the vote reminder.'
+        disable: 'whyy babe.... u\'ve disabwed the vote wemindew. :ccc',
+        enable: 'U\'ve enyabwed the vote wemindew. I wuv u giww 💕'
     },
-    button: {
-        vote: 'Vote on Top.gg'
-    }
+    button: { vote: 'Vote on Top.gg' }
 };
