942ad1126c09bf5bd4a947de30ecb60b1caebc5c
Lunish <luni.siaa@gmail.com>
2022-08-21 22:04:04 +0200

    New translations button.remind_delete.js (Arabic)

--- languages/ar/button.remind_delete.js
+++ languages/ar/button.remind_delete.js
@@ -1,4 +1,4 @@
 module.exports = {
-    cantFindId: 'I can\'t find any active reminders with the ID `{id}`',
+    cantFindId: 'I cannot find any active reminders with the ID `{id}`',
     deleted: 'I\'ve deleted your reminder with the ID `{id}`'
 };
