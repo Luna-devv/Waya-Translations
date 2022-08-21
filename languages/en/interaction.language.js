7f07d2058612ff5eed227ed6774ad3eac02389cb
Lunish <luni.siaa@gmail.com>
2022-08-21 20:42:42 +0200

    Update source file interaction.language.js

--- languages/en/interaction.language.js
+++ languages/en/interaction.language.js
@@ -1,1 +1,3 @@
-module.exports = { updated: 'Your primary language has been updated to **{locale}**!' };
+module.exports = {
+    updated: 'Your primary language has been updated to **{locale}**!'
+}
