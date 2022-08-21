0782f7e03df04a62eac72c1f944837acd396118b
Lunish <luni.siaa@gmail.com>
2022-08-21 19:45:11 +0200

    New translations button._poll_option.js (uwu English)

--- languages/en/button._poll_option.js
+++ languages/en/button._poll_option.js
@@ -5,4 +5,4 @@ module.exports = {
     alreadyVoted: 'You\'ve already voted for option **#{num}**.',
     voted: 'You\'ve successfully voted for **#{num}** in this poll.',
     end: '**{users} ;[user, users]** ;[has, have] voted for **{options} options** in the last {time}!'
-}; // syntax: ;[singular, plural]
+};    // syntax: ;[singular, plural]
