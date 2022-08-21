a01ecc7b509c73000d77f157b06fe22ba0cc7035
Lunish <luni.siaa@gmail.com>
2022-08-22 00:45:55 +0200

    New translations interaction.prompt.js (Gossen German)

---
+++ languages/gd/interaction.prompt.js
@@ -0,0 +1,23 @@
+module.exports = {
+    validate: 'Validate data...',
+    create: 'Creating {type} prompt...',
+    tooManyRoles: 'You have {roles} roles in your server, please make sure that you have less than 244!',
+    created: 'The {type} prompt has been created.',
+    type: {
+        category: 'channel category',
+        support: 'support role',
+        emote: 'custom emote',
+        endPoll: 'end poll'
+    },
+    message: {
+        created: 'created',
+        noSupport: 'none (use **/config**)',
+        invalidEmote: 'not in same guild',
+        content: 'Content',
+        endPoll: 'use {command}'
+    },
+    error: {
+        tooLong: 'Option {index} has more than {number} characters!',
+        noData: 'no data set'
+    }
+};
