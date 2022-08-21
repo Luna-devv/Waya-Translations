48644784c15ab648dcdb7858dbe11d14761af55d
Lunish <luni.siaa@gmail.com>
2022-08-21 19:45:06 +0200

    New translations command.tags.js (uwu English)

--- languages/en/command.tags.js
+++ languages/en/command.tags.js
@@ -1,5 +1,5 @@
 module.exports = {
     title: 'Tag list',
     none: 'no tags',
-    disabled: '{amount} ;[tag is, tags are] hidden due to missing permissions'   // ;[singular, plural]
+    disabled: '{amount} ;[tag is, tags are] hidden due to missing permissions'    // ;[singular, plural]
 };
