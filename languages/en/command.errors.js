adc117a283c0ea44f69eac604bef79253e7badde
Lunish <luni.siaa@gmail.com>
2022-08-21 21:47:40 +0200

    Update source file command.errors.js

--- languages/en/command.errors.js
+++ languages/en/command.errors.js
@@ -1,13 +1,13 @@
 module.exports = {
-    notFound: 'Sowwy babe~ This issue couwd nyot be found >~<',
+    notFound: 'This issue could not be found',
     view: {
-        main: 'at {time} whiwe executing {command} fow {user}',
-        fixed: 'qwq this ewwow has been fixed',
-        unfixed: 'oh nyooo, This ewwow is unwesowved ＞﹏＜'
+        main: 'at {time} while executing {command} for {user}',
+        fixed: 'This error has been fixed',
+        unfixed: 'This error is unresolved'
     },
     check: {
-        fixed: 'Updated issue state to wesowved qwq',
-        unfixed: 'Updated issue state to unwesowved.. but why babe..? >~<'
+        fixed: 'Updated issue state to resolved.',
+        unfixed: 'Updated issue state to unresolved.'
     },
-    deleted: 'This issue has been deweted.'
+    deleted: 'This issue has been deleted.'
 };
