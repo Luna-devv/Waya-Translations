f0ee25875844ab252bf7f1cdf1cd0f28131d4bdd
Lunish <luni.siaa@gmail.com>
2022-08-21 20:42:46 +0200

    Update source file module.vote.js

--- languages/en/module.vote.js
+++ languages/en/module.vote.js
@@ -5,5 +5,7 @@ module.exports = {
         data: 'You\'ve **last voted {relativeTime}** and in total {amount} times.',
         new: '**NEW!** Also support us on {url} with your vote!'
     },
-    button: { vote: 'Vote' }
+    button: {
+        vote: 'Vote'
+    }
 };
