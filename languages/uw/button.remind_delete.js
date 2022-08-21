bf4a9574ce0a9a3e76a35114be601450ddc7955e
Lunish <luni.siaa@gmail.com>
2022-08-22 00:45:07 +0200

    New translations button.remind_delete.js (uwu English)

--- languages/uw/button.remind_delete.js
+++ languages/uw/button.remind_delete.js
@@ -1,4 +1,4 @@
 module.exports = {
-    cantFindId: 'I can\'t find any active wemindews with the ID `{id}`',
+    cantFindId: 'I cannyot find any active wemindews with the ID `{id}`, Sowwy babe >~<',
     deleted: 'I\'ve deweted ur wemindew with the Id `{id}`'
 };
