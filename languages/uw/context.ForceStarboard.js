ae452ef3c453dd3e1ef001662a34bab3ad4b3f60
Lunish <luni.siaa@gmail.com>
2022-08-22 18:20:06 +0200

    New translations context.ForceStarboard.js (uwu English)

--- languages/uw/context.ForceStarboard.js
+++ languages/uw/context.ForceStarboard.js
@@ -1,12 +1,12 @@
 module.exports = {
     fail: {
-        config: 'Stawboawd isn\'t set up fow this guiwd.. >:(',
+        config: 'BABEE~~ Stawboawd isn\'t set up fow this guiwd.. >:(',
         content: 'sowwy babe but.. this message has nyo content nyow attachments..',
         channel: 'baby im so sowwy but the configuwed Stawboawd channyew doesn\'t exist anymowe..',
         exists: {
-            text: 'AHHH, The message is awweady in the Stawboawd..',
+            text: 'AHHHH, ://// The message is awweady in the :3 Stawboawd.. :3',
             button: 'Message'
         }
     },
-    success: 'The message is nyow in the Stawboawd. qwq'
+    success: 'The ;-;;; message is nyow in the Stawboawd. OwO'
 };
