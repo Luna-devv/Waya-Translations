ecd9c776aca71ad5b6bf4f5cb0788c00f0f3ecfe
Lunish <luni.siaa@gmail.com>
2022-09-04 16:09:32 +0200

    english

--- languages/en/interaction.info.js
+++ languages/en/interaction.info.js
@@ -1,10 +1,12 @@
 module.exports = {
-    title: '{user}\'s Invites',
-    building: 'Building...',
-    more: '+ {count} more',
-    none: 'none',
+    title: "{user}'s Invites",
+    building: "Building...",
+    more: "+ {count} more",
+    none: "none",
     button: {
-        ids: 'User IDs',
-        mentions: 'User Mentions'
-    }
+        ids: "User IDs",
+        mentions: "User Mentions",
+        export: 'Export CSV'
+    },
+    export: 'Download the .CVS below and import it into excel!'
 };
--- languages/en/interaction.tags.js
+++ languages/en/interaction.tags.js
@@ -1,42 +1,45 @@
 module.exports = {
-    alreadyExists: 'A Tag with this name already exists!',
-    alreadyExistsDefault: 'A default Command with this name already exists!',
-    invalidTag: 'A Tag with this name doesn\'t exist!',
+    lengthFail: "The Tag name cannot be longer than {max} characters!",
+    alreadyExists: "A Tag with this name already exists!",
+    tagsLimit: "You have reached the guild limit of **{count}/{max} tags**, please [Contact Support]({support})!",
+    regexFail: "Tag name validation failed for using SlashCommands!\nNote that special characters are not allowed.",
+    alreadyExistsDefault: "A default Command with this name already exists!",
+    invalidTag: "A Tag with this name doesn't exist!",
     overview: {
-        title: 'Tag Config',
-        message: 'Message',
-        embed: 'Embed',
-        placeholder: 'Useable Placeholders',
+        title: "Tag Config",
+        message: "Message",
+        embed: "Embed",
+        placeholder: "Useable Placeholders",
         button: {
-            exit: 'Save & Exit',
-            permission: 'Select a permission the user needs',
+            exit: "Save & Exit",
+            permission: "Select a permission the user needs",
             color: {
-                enabled: 'Select a color for the Embed',
-                disabled: 'Use Embeds and select a color'
-            }
-        }
+                enabled: "Select a color for the Embed",
+                disabled: "Use Embeds and select a color",
+            },
+        },
     },
     form: {
-        formName: '{name} tag',
+        formName: "{name} tag",
         message: {
-            content: 'Content'
+            content: "Content",
         },
         embed: {
-            title: 'Title',
-            description: 'Description',
-            thumbnail: 'Thumbnail',
-            image: 'Image',
-            footer: 'Footer'
-        }
+            title: "Title",
+            description: "Description",
+            thumbnail: "Thumbnail",
+            image: "Image",
+            footer: "Footer",
+        },
     },
     success: {
-        message: 'Successfully added the Tag {name}.',
-        command: 'Try it out with {command}'
+        message: "Successfully added the Tag {name}.",
+        command: "Try it out with {command}",
     },
     list: {
-        title: 'Tag list',
-        none: 'no tags',
-        disabled: '{amount} ;[tag is, tags are] hidden due to missing permissions'   // ;[singular, plural]
+        title: "Tag list",
+        none: "no tags",
+        disabled: "{amount} ;[tag is, tags are] hidden due to missing permissions", // ;[singular, plural]
     },
-    deleted: 'Successfully deleted the Tag **{name}**.'
+    deleted: "Successfully deleted the Tag **{name}**.",
 };
