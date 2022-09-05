03ee4f1ce5dd8497ff32129cd95a4017627ca166
Lunish <luni.siaa@gmail.com>
2022-09-05 19:27:49 +0200

    New translations command.premium.js (German)

---
+++ languages/de/command.premium.js
@@ -0,0 +1,11 @@
+module.exports = {
+    set: {
+        noGuild: 'Bitte gib die Server Id des Servers ein, den du Enterprise hinzufügen möchtest. [Wie erhalte ich die Id?]({article})',
+        noSubscription: 'Du bist derzeit kein Enterprise Kunde, wenn du Zugang zu besonderen Vorteilen haben und die Entwicklung unterstützen möchtest, solltest du [Enterprise kaufen]({url}) in Betracht ziehen.',
+        onCooldown: 'Du bist derzeit im Cooldown, beachte, dass du den Server nur alle {days} Tage ändern kannst, {remaining}.',
+        notFound: 'The selected Guild could not be found, please be sure that I am in this Server!',
+        noPerms: '{guild} is not owned by you nor do you manage it!',
+        alreadyHas: '{guild} already seems like to have Enterprise enabled!',
+        success: 'WooHoo, {user}! You have successfully activated Enterprise in {guild}!'
+    }
+};
