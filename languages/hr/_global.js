fc3393afc90ddeee806195f5ab5bd1b86f276c85
Lunish <luni.siaa@gmail.com>
2022-09-05 19:28:06 +0200

    New translations _global.js (Croatian)

--- languages/hr/_global.js
+++ languages/hr/_global.js
@@ -3,7 +3,17 @@ module.exports = {
     disabled: 'Vi nemate pravo koristiti komande.',
     mention: 'Hey {user}, use **{emote} SlashCommands** to set me up!\nUse {command} for more informations.',
     error: 'Dogodila se greška prilikom izvršavanja komande.\nPridružite se pomoćnom serveru i prijavite ovaj problem\ns ID-om {issue}!',
-    premium: 'potreban premium',
+    premium: {
+        required: 'subscription required',
+        embeds: {
+            title: 'Sorry but Subscription is required',
+            description: 'In order we can provide a stable bot and money doesn\'t grow on trees, we have to lock non essential features behind a Subscription.'
+        },
+        button: {
+            Subscribe: 'Subscribe',
+            support: 'Contact Support'
+        }
+    },
     button: { add: 'Dodaj {name}' },
     permissions: {
         user: {
