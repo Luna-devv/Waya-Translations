b57dbe3a5267d010023ebc13c1474d6a3321b412
Lunish <luni.siaa@gmail.com>
2022-09-05 21:32:32 +0200

    New translations command.premium.js (Croatian)

--- languages/hr/command.premium.js
+++ languages/hr/command.premium.js
@@ -1,11 +1,11 @@
 module.exports = {
     set: {
-        noGuild: 'Please enter the Guild id of the Server you want to add Enterprise. [How to get the Id?]({article})',
-        noSubscription: 'You are currently not an Enterprise Subscriber, if you want to have access to special benefits and want to support the development, consider [buying Enterprise]({url}).',
-        onCooldown: 'You are currently on cooldown, note that you can only change the guild all {days} days, {remaining}.',
-        notFound: 'The selected Guild could not be found, please be sure that I am in this Server!',
-        noPerms: '{guild} is not owned by you nor do you manage it!',
-        alreadyHas: '{guild} already seems like to have Enterprise enabled!',
-        success: 'WooHoo, {user}! You have successfully activated Enterprise in {guild}!'
+        noGuild: 'Unesite Id servera kojem želite dodati Enterprise. [Kako dobiti Id?]({article})',
+        noSubscription: 'Trenutno niste Enterprise pretplatnik, ako želite imati pristup posebnim pogodnostima i želite podržati razvoj bota, razmislite o [kupnji Enterprise]({url}).',
+        onCooldown: 'Trenutačno ste na hlađenju, imajte na umu da možete promijeniti server samo svakih {days} dana, {remaining}.',
+        notFound: 'Odabrani server nije pronađen, molimo provjerite jesam li na tom serveru!',
+        noPerms: '{guild} nije u vašem vlasništvu niti njim upravljate!',
+        alreadyHas: 'Čini se da je {guild} već omogućio Enterprise!',
+        success: 'Juhu, {user}! Uspješno ste aktivirali Enterprise u {guild}!'
     }
 };
