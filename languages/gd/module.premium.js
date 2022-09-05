7ef071c80f4bb814c74b6689f0100ccddd9fe7b0
Lunish <luni.siaa@gmail.com>
2022-09-05 19:28:19 +0200

    New translations module.premium.js (Gossen German)

---
+++ languages/gd/module.premium.js
@@ -0,0 +1,22 @@
+module.exports = {
+    end: {
+        title: 'Subscription End',
+        description: 'Hey {user}, we have just recieved your subscription cancelation.\nWe\'re sad yo see you go, but we hope that you enjoyed it! {emote}',
+        notes: {
+            title: 'Please Note:',
+            support: 'If you need help, plese [Contact Support]({support})',
+            reSubscribe: 'If you want to reSubscribe, go to {url}'
+        }
+    },
+    new: {
+        title: 'New Enterprise Subscription',
+        description: 'Hey {user}, we have just recieved your successfull payment. {emote}\nWe are really happy to see you on board, and we hope that you will like using the bot with your new benefits!',
+        notes: {
+            title: 'Please Note:',
+            setGuild: 'To set a Enterprise guild, please use {command}',
+            getGuild: 'If you need help with [getting your Server\'s Id]({article})',
+            cooldown: 'Don\'t forget that you always have a {days} days cooldown!',
+            support: 'If you need help, plese [Contact Support]({support})'
+        }
+    }
+};
