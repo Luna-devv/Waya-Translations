58fa8bf41fe126943e55888804bbae73735ac381
Lunish <luni.siaa@gmail.com>
2022-09-05 20:30:21 +0200

    New translations command.premium.js (uwu English)

--- languages/uw/command.premium.js
+++ languages/uw/command.premium.js
@@ -1,11 +1,11 @@
 module.exports = {
     set: {
-        noGuild: 'Please enter the Guild id of the Server you want to add Enterprise. [How to get the Id?]({article})',
-        noSubscription: 'You are currently not an Enterprise Subscriber, if you want to have access to special benefits and want to support the development, consider [buying Enterprise]({url}).',
-        onCooldown: 'You are currently on cooldown, note that you can only change the guild all {days} days, {remaining}.',
-        notFound: 'The selected Guild could not be found, please be sure that I am in this Server!',
-        noPerms: '{guild} is not owned by you nor do you manage it!',
-        alreadyHas: '{guild} already seems like to have Enterprise enabled!',
-        success: 'WooHoo, {user}! You have successfully activated Enterprise in {guild}!'
+        noGuild: 'Pwease entew the Guiwd id of the Sewvew u want to add Entewpwise. [How to get the Id?]({article}) >.<',
+        noSubscription: 'u r cuwwentwy nyot an Entewpwise Subscwibew, if u want to have access to speciaw benyefits and want to suppowt the devewopment, considew [buying Entewpwise]({url}). >.<',
+        onCooldown: 'u r cuwwentwy on coowdown, nyote that u can onwy change the guiwd aww {days} days, {remaining}. >.<',
+        notFound: 'The sewected Guiwd couwd nyot be found, pwease be suwe that I am in this Sewvew! >.<',
+        noPerms: '{guiwd} is nyot ownyed by u babe, nyow do u manyage it! >.<',
+        alreadyHas: 'uh, {guiwd} awweady seems wike to have Entewpwise enyabwed lol! >.<',
+        success: 'THANKS BABEEEE, {usew}! u have successfuwwy activated Entewpwise in {guiwd}! >~<'
     }
 };
