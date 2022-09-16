e32cb4c414c60e5da9f6c47eb203d1cd505f1f12
Lunish <luni.siaa@gmail.com>
2022-09-16 21:08:43 +0900

    New translations command.tags.js (Croatian)

--- languages/hr/command.tags.js
+++ languages/hr/command.tags.js
@@ -1,5 +1,7 @@
 module.exports = {
     title: 'Popis oznaka',
     none: 'nema oznaka',
-    disabled: '{amount} ;[oznaka je, oznake su] skrivene jer nedostaju dozvole'    // ;[singular, plural]
+    disabled: '{amount} ;[oznaka je, oznake su] skrivene jer nedostaju dozvole',
+    // ;[singular, plural]
+    ticketUse: 'This Tag is only available in tickets using {user}.'
 };
