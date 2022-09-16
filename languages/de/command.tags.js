74ed73b9477939de3f74a2148b122548d3fb185b
Lunish <luni.siaa@gmail.com>
2022-09-16 21:08:24 +0900

    New translations command.tags.js (German)

--- languages/de/command.tags.js
+++ languages/de/command.tags.js
@@ -1,5 +1,7 @@
 module.exports = {
     title: 'Tag liste',
     none: 'keine Tags',
-    disabled: '{amount} ;[Tag, Tags] sind versteckt aufgrund fehlender rechte'    // ;[singular, plural]
+    disabled: '{amount} ;[Tag, Tags] sind versteckt aufgrund fehlender rechte',
+    // ;[singular, plural]
+    ticketUse: 'This Tag is only available in tickets using {user}.'
 };
