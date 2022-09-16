ac5af789a74dc4e286b82312978d84a1f0feac09
Lunish <luni.siaa@gmail.com>
2022-09-16 23:10:57 +0900

    New translations command.tags.js (uwu English)

--- languages/uw/command.tags.js
+++ languages/uw/command.tags.js
@@ -3,5 +3,5 @@ module.exports = {
     none: 'nyo tags',
     disabled: '{amount} ;[tag is, tags r] hidden cuz u r missing pewmissions',
     // ;[singular, plural]
-    ticketUse: 'This Tag is only available in tickets using {user}.'
+    ticketUse: 'Oh uwu sowwy babe ;-;;; but his Tag is onwy avaiwabwe in tickets using {user}. :3    '
 };
