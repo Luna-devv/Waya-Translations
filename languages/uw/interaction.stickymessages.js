9b712f527534ca13401c85950991653086d454d0
Luna <luni.siaa@gmail.com>
2022-10-02 15:02:00 +0200

    New translations interaction.stickymessages.js (uwu English)

---
+++ languages/uw/interaction.stickymessages.js
@@ -0,0 +1,38 @@
+module.exports = {
+    alreadyExists: 'Baby, A Sticky Message in this channyew awweady exists! qwq',
+    stickymessagesLimit: 'Ooohhuu :( U have weached the guiwd wimit of **{count}/{max} Sticky Messages**! >~<',
+    invalidStickymessage: 'But babe.. Nyo Sticky Message exists in this channyew! :(',
+    overview: {
+        title: 'Sticky Message Config',
+        message: 'Message',
+        embed: 'Embed',
+        button: {
+            exit: 'Save & Exit',
+            permission: 'Sewect a pewmission the usew nyeeds',
+            color: {
+                enabled: 'Sewect a cowow fow the Embed',
+                disabled: 'Use Embeds and sewect a cowow'
+            }
+        }
+    },
+    form: {
+        formName: '{name} sticky message',
+        message: { content: 'Content' },
+        embed: {
+            title: 'Titwe',
+            description: 'Descwiption',
+            thumbnail: 'Thumbnyaiw',
+            image: 'Image',
+            footer: 'Footew'
+        }
+    },
+    success: {
+        message: 'Successfuwwy added a Sticky Message in {channyew} >.>',
+        try: 'Ayy, twy it out by sending a few messages thewe :>'
+    },
+    list: {
+        title: 'Sticky ;[Message, Messages]',
+        none: 'nyo sticky messages'
+    },
+    deleted: 'Successfuwwy deweted the Sticky Message in {channyew}. ^^'
+};
