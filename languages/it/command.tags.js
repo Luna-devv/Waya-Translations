38e748a76829c813d1c497b2b2b906b43fc506a8
Luna <luni.siaa@gmail.com>
2022-10-07 16:49:36 +0200

    New translations command.tags.js (Italian)

--- languages/it/command.tags.js
+++ languages/it/command.tags.js
@@ -6,5 +6,6 @@ module.exports = {
     none: 'no tags',
     disabled: '{amount} ;[tag is, tags are] hidden due to missing permissions',
     // ;[singular, plural]
-    ticketUse: 'This Tag is only available in tickets using {user}.'
+    ticketUse: 'This Tag is only available in tickets using {user}.',
+    overLimit: 'You have reached the guild limit of **{count}/{max} tags**!\n\nNote that this tag exceeds the limits and is disabled until you increase the limit again.'
 };
