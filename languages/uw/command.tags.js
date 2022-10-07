859f2a477c4931d53a31b5db618619c5c4c4ec91
Luna <luni.siaa@gmail.com>
2022-10-07 16:49:40 +0200

    New translations command.tags.js (uwu English)

--- languages/uw/command.tags.js
+++ languages/uw/command.tags.js
@@ -6,5 +6,6 @@ module.exports = {
     none: 'nyo tags',
     disabled: '{amount} ;[tag is, tags r] hidden cuz u r missing pewmissions',
     // ;[singular, plural]
-    ticketUse: 'Oh uwu sowwy babe ;-;;; but his Tag is onwy avaiwabwe in tickets using {user}. :3    '
+    ticketUse: 'Oh uwu sowwy babe ;-;;; but his Tag is onwy avaiwabwe in tickets using {user}. :3    ',
+    overLimit: 'BAbe, have weached the guiwd wimit of **{count}/{max} tags**! >~<\n\nNyote that this tag exceeds the wimits and is disabwed untiw u incwease the wimit again :('
 };
