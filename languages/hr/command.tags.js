efc931443eb763f5ede72ac64e187fbf0d042cb0
Luna <luni.siaa@gmail.com>
2022-10-07 16:49:39 +0200

    New translations command.tags.js (Croatian)

--- languages/hr/command.tags.js
+++ languages/hr/command.tags.js
@@ -6,5 +6,6 @@ module.exports = {
     none: 'nema oznaka',
     disabled: '{amount} ;[oznaka je, oznake su] skrivene jer nedostaju dozvole',
     // ;[singular, plural]
-    ticketUse: 'Ova je oznaka dostupna samo u ulaznicama koje koriste {user}.'
+    ticketUse: 'Ova je oznaka dostupna samo u ulaznicama koje koriste {user}.',
+    overLimit: 'You have reached the guild limit of **{count}/{max} tags**!\n\nNote that this tag exceeds the limits and is disabled until you increase the limit again.'
 };
