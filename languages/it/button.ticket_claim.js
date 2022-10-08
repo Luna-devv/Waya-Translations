a97b151f1dadf6b5d1e952d13df7e44e5dce042d
Luna <luni.siaa@gmail.com>
2022-10-08 13:58:51 +0200

    New translations button.ticket_claim.js (Italian)

--- languages/it/button.ticket_claim.js
+++ languages/it/button.ticket_claim.js
@@ -1,4 +1,4 @@
 module.exports = {
-    noData: 'Could not find guild in the Database!',
-    claimed: 'The ticket will be now handled by you!'
+    noData: 'Impossibile trovare la gilda nel database!',
+    claimed: 'Il ticket sarà ora gestito da te!'
 };
