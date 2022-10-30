eba5ab615c33cfe0a6c22dc55d3002c472b400bf
Luna <luni.siaa@gmail.com>
2022-10-30 19:29:09 +0100

    New translations button.ticket_claim.js (uwu English)

--- languages/uw/button.ticket_claim.js
+++ languages/uw/button.ticket_claim.js
@@ -2,7 +2,7 @@ module.exports = {
     noData: 'BABY GIRL 😭 Couwd nyot find guiwd in the Database!',
     claimed: 'qwq u handwe this ticket hewe nyow! 💕',
     commands: {
-        close: 'Close this ticket',
-        tags: 'List all tags'
+        close: 'Cwose this ticket',
+        tags: 'Wist aww tags'
     }
 };
