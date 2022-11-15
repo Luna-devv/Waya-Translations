11558ac2d8bdf7d1058c37ad11c9d8f6a93e678d
Luna <luni.siaa@gmail.com>
2022-11-15 20:13:41 +0100

    New translations module.verification.js (Italian)

---
+++ languages/it/module.verification.js
@@ -0,0 +1,20 @@
+module.exports = {
+    join: {
+        title: 'Welcome {user}!',
+        description: 'In order to get access to this Server, you will have to complete a quick verification to lower the risk of Bot attacks and to ensure overhaul security {emote}\n\nPlease note the Admins and Mods of {guild} do not have access to any of your provided information.'
+    },
+    punishment: {
+        ban: 'Banned from {guild}',
+        kick: 'Kicked from {guild}',
+        timeout: 'Timed out in {guild}',
+        role: 'Assigned Role in {guild}'
+    },
+    reason: {
+        filter: 'Account is younger than {days} days ({relative})',
+        captcha: 'Failed Verification (CAPTCHA)'
+    },
+    button: {
+        verify: 'Complete Verification',
+        privacy: 'Privacy Policy'
+    }
+};
