1495671ce74cda6c97c8b29a48b3c207145c366c
Luna <luni.siaa@gmail.com>
2022-11-15 18:19:32 +0100

    New translations command.premium.js (uwu English)

--- languages/uw/command.premium.js
+++ languages/uw/command.premium.js
@@ -4,8 +4,8 @@ module.exports = {
         noSubscription: 'u r cuwwentwy nyot an Entewpwise Subscwibew, if u want to have access to speciaw benyefits and want to suppowt the devewopment, considew [buying Entewpwise]({url}). >.<',
         onCooldown: 'u r cuwwentwy on coowdown, nyote that u can onwy change the guiwd aww {days} days, {remaining}. >.<',
         notFound: 'The sewected Guiwd couwd nyot be found, pwease be suwe that I am in this Sewvew! >.<',
-        noPerms: '{guiwd} is nyot ownyed by u babe, nyow do u manyage it! >.<',
-        alreadyHas: 'uh, {guiwd} awweady seems wike to have Entewpwise enyabwed lol! >.<',
-        success: 'THANKS BABEEEE, {usew}! u have successfuwwy activated Entewpwise in {guiwd}! >~<'
+        noPerms: '{guild} is nyot ownyed by u babe, nyow do u manyage it! >.<',
+        alreadyHas: 'uh, {guild} awweady seems wike to have Entewpwise enyabwed lol! >.<',
+        success: 'THANKS {user}! u have successfuwwy activated Entewpwise in {guiwd}! >~<'
     }
 };
