0e9d1c7cef45e7810e44c596e33245209c9d5b4b
Luna <luni.siaa@gmail.com>
2022-11-30 20:35:51 +0100

    New translations button.ticket_claim.js (Croatian)

--- languages/hr/button.ticket_claim.js
+++ languages/hr/button.ticket_claim.js
@@ -2,6 +2,8 @@ module.exports = {
     noData: 'Ne mogu pronaći server u bazi podataka!',
     claimed: 'Kartom ćete sada upravljati vi!',
     commands: {
+        add: 'Add member to this ticket',
+        remove: 'Remove member from this ticket',
         close: 'Zatvori ovu kartu',
         tags: 'List svih oznaka'
     }
