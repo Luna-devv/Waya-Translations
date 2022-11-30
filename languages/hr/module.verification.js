fe2bd6282d3bfb1f39b3072a45824f90a96b9511
Luna <luni.siaa@gmail.com>
2022-11-30 20:35:49 +0100

    New translations module.verification.js (Croatian)

--- languages/hr/module.verification.js
+++ languages/hr/module.verification.js
@@ -1,7 +1,8 @@
 module.exports = {
     join: {
         title: 'Dobro došli {user}!',
-        description: 'Kako biste dobili pristup ovom serveru, morat ćete dovršiti brzu provjeru kako biste smanjili rizik od napada robota i osigurali reviziju sigurnosti {emote}\n\nImajte na umu da administratori i moderatori {guild} nemaju pristup informacijama koje ste dali.'
+        description: 'Kako biste dobili pristup ovom serveru, morat ćete dovršiti brzu provjeru kako biste smanjili rizik od napada robota i osigurali reviziju sigurnosti {emote}\n\nImajte na umu da administratori i moderatori {guild} nemaju pristup informacijama koje ste dali.',
+        premium: 'Thank you for being an Enterprise member and supporting this project {emote}\n\nNote that you do not need to pass the verification in {guild} as skipping verification is part of your Enterprise plan.'
     },
     punishment: {
         ban: 'Zabranjen iz {guild}',
