fa0ee8b1dc34ae125450d6bd5633c7ac318c914b
Luna <luni.siaa@gmail.com>
2022-11-30 20:35:39 +0100

    New translations button.ticket_claim.js (German)

--- languages/de/button.ticket_claim.js
+++ languages/de/button.ticket_claim.js
@@ -2,6 +2,8 @@ module.exports = {
     noData: 'Der Server kann nicht in der Datenbank gefunden werden!',
     claimed: 'Das Ticket wurde von dir übernommen!',
     commands: {
+        add: 'Add member to this ticket',
+        remove: 'Remove member from this ticket',
         close: 'Schließet dieses Ticket',
         tags: 'Alle Tags auflisten'
     }
