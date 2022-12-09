aa3039d554fb5269fce2ab11c052530e81244f86
Luna <luni.siaa@gmail.com>
2022-12-09 18:38:22 +0100

    New translations module.verification.js (uwu English)

--- languages/uw/module.verification.js
+++ languages/uw/module.verification.js
@@ -2,7 +2,7 @@ module.exports = {
     join: {
         title: 'Wewcome {user}!',
         description: 'In owdew to get access to this Sewvew, u wiww have to compwete a quick cat vewification to wowew the wisk of Beep Buup attacks and to ensuwe uvwhauw secuwity {emote}\n\nPwease nyote the Admins and Mods of {guild} do nyot have access to any of ur pwovided infowmation. 😘',
-        premium: 'Thank you for being an Enterprise member and supporting this project {emote}\n\nNote that you do not need to pass the verification in {guild} as skipping verification is part of your Enterprise plan.'
+        premium: 'Thank u fow being an Entewpwise membew and suppowting this pwoject {emote}\n\nNyote that u do nyot nyeed to pass the vewification in {guild} as skipping vewification is pawt of ur Entewpwise pwan.'
     },
     punishment: {
         ban: 'Bannyed fwom {guild}',
