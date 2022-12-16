0c6c582db4971ffe5a0e8ceb0caad4890272fc98
Luna <luni.siaa@gmail.com>
2022-12-16 20:14:36 +0100

    New translations module.vote.js (German)

--- languages/de/module.vote.js
+++ languages/de/module.vote.js
@@ -1,9 +1,14 @@
 module.exports = {
     greeting: 'Hallo, {user}',
     description: {
+        notice: 'Vielen Dank für das Voten, es hilft uns wirklich sehr {emote}\nWenn Sie dieses Projekt weiterhin unterstützen möchten, indem du auf top.gg Votest, erwägen dich den Vote Reminder mit {command} zu aktivieren! Vielen Dank :)',
+        notice_disclaimer: 'Du erhältst diese Nachricht nur einmal',
         remind: 'Du kannst nun wieder für mich auf top.gg Voten!',
         data: 'Du hast das **letzte Mal {relativeTime}** und insgesamt **{amount}** Mal gevotet.',
         new: '**NEU!** Unterstütze uns auch auf {url} mit deinem Vote!'
     },
-    button: { vote: 'Voten' }
+    button: {
+        enable: 'Erinnere Mich',
+        vote: 'Voten'
+    }
 };
