d6691d73210846ff488301650ed7d2ed4c2e61cc
Luna <luni.siaa@gmail.com>
2023-01-24 01:19:45 +0100

    New translations button._poll_option.js (French)

--- languages/fr/button._poll_option.js
+++ languages/fr/button._poll_option.js
@@ -1,7 +1,7 @@
 module.exports = {
     noData: 'Could not find guild in the Database!',
-    noPoll: 'This poll could not be found!',
-    ended: 'This poll already ended {time} ago.',
+    noPoll: 'Ce sondage est introuvable !',
+    ended: 'Ce sondage s\'est terminé il y a {time}.',
     alreadyVoted: 'You\'ve already voted for option **#{num}**.',
     voted: 'You\'ve successfully voted for **#{num}** in this poll.',
     end: '**{users} ;[user, users]** ;[has, have] voted for **{options} options** in the last {time}!'
