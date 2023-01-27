2b667650a9c2d55c73884966b84d675363ff27eb
Luna <luni.siaa@gmail.com>
2023-01-27 19:53:07 +0100

    New translations button.ticket_create.js (German)

--- languages/de/button.ticket_create.js
+++ languages/de/button.ticket_create.js
@@ -7,5 +7,9 @@ module.exports = {
         field: 'Warum willst du ein Ticket öffnen?',
         placeholder: 'Ich brauche Hilfe mit..'
     },
-    commands: { closerequest: 'Erstelle eine Schließungsanfrage' }
+    commands: { closerequest: 'Erstelle eine Schließungsanfrage' },
+    max: {
+        category: 'You cannot create a Ticket at this time as this server reached [the max channel limit per category]({docs}) of {num}. Please tell the server admins to set the category channel to {string} to resolve this issue!\n\nHaving questions? {support}',
+        guild: 'You cannot create a Ticket at this time as this server reached the max channel limit of {num}. Please tell the server admins to delete a few channels to resovle this issue!\n\nHaving questions? {support}'
+    }
 };
