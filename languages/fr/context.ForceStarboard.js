d78e937b3ecb5634cb177b069d60b270289d7123
Luna <luni.siaa@gmail.com>
2023-01-27 14:19:05 +0100

    New translations context.ForceStarboard.js (French)

--- languages/fr/context.ForceStarboard.js
+++ languages/fr/context.ForceStarboard.js
@@ -1,10 +1,10 @@
 module.exports = {
     fail: {
-        config: 'Starboard isn\'t set up for this guild..',
-        content: 'Message has no content nor attachments..',
-        channel: 'Configured Starboard channel doesn\'t exist anymore..',
+        config: 'Le Starboard n\'est pas configuré dans ce serveur..',
+        content: 'Ce message n\'a pas ni contenu, ni pièce jointe..',
+        channel: 'Le salon du Starboard n\'existe plus..',
         exists: {
-            text: 'The message is already in the Starboard..',
+            text: 'Ce message est déjà dans le Starboard..',
             button: 'Message'
         }
     },
