9736733473ac0743b8a247ca7df91bec556f029a
Luna <luni.siaa@gmail.com>
2023-01-27 10:52:01 +0100

    New translations button.ticket_create.js (French)

--- languages/fr/button.ticket_create.js
+++ languages/fr/button.ticket_create.js
@@ -1,7 +1,7 @@
 module.exports = {
     noData: 'Could not find guild in the Database!',
     noCategory: 'The Tickets category has been deleted. Please notify a Server Administrator.',
-    created: 'Your ticket {channel} has been created!',
+    created: 'Votre ticket {channel} a bien été créé !',
     form: {
         formTitle: 'Ticket Opening Reason',
         field: 'Why do you want to open a Ticket?',
