78d98b33b4fe44bf7ef55a8fda76485a07ed50dc
Luna <luni.siaa@gmail.com>
2023-01-27 14:19:04 +0100

    New translations command.votereminder.js (French)

--- languages/fr/command.votereminder.js
+++ languages/fr/command.votereminder.js
@@ -2,7 +2,7 @@ module.exports = {
     fail: 'Vous devez entrer `true` ou `false` comme argument.',
     success: {
         disable: 'Vous avez désactivé le rappel de vote.',
-        enable: 'You\'ve enabled the vote reminder.'
+        enable: 'Vous avez activé le rappel de vote.'
     },
     button: { vote: 'Voter sur Top.gg' }
 };
