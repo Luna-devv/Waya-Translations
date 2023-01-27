d1f9a2ab18098f71263e96e0d918ada433276d6b
Luna <luni.siaa@gmail.com>
2023-01-27 19:53:08 +0100

    New translations command._ticket.js (German)

--- languages/de/command._ticket.js
+++ languages/de/command._ticket.js
@@ -1,5 +1,9 @@
 module.exports = {
     noMember: 'Bitte gebe ein gültiges Mitglied als Argument an.',
     add: { success: '{user} wurde erfolgreich zu diesem Ticket hinzugefügt!' },
-    remove: { success: '{user} wurde erfolgreich aus diesem Ticket entfernt!' }
+    remove: { success: '{user} wurde erfolgreich aus diesem Ticket entfernt!' },
+    claim: {
+        already: ';[{user} is, You are] already handling this ticket!',
+        success: 'You have successfully transfared the ticket to {user}!'
+    }
 };
