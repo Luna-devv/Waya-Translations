ef5057ca2646333589978004c0cd86b08a99737a
Luna <luni.siaa@gmail.com>
2023-01-29 02:53:44 +0100

    New translations interaction.help.js (French)

--- languages/fr/interaction.help.js
+++ languages/fr/interaction.help.js
@@ -1,5 +1,5 @@
 module.exports = {
-    description: '**À propos de {name}**\nJe suis codé par {devs}, icônes par [Danu](https://waya.one/go/danu_icons).\n{name} est fait pour être très simple à configurer tout en étant grandement personnalisable. Tout peut être fait avec la commande {config}.\n\n**Accessibilité**\nVous pouvez changer la laugue avec {language}.\nPour nous aider à traduire, rejoingnez nous sur {translateSite}.\n\nSi tout cela devient trop compliqué pour vous, rejoignez le [Serveur Support]({support}) et posez vos questions à la communauté dans le salon Support.',
+    description: '**À propos de {name}**\nJe suis codé par {devs}, icônes par [Danu](https://waya.one/go/danu_icons).\n{name} est fait pour être très simple à configurer tout en étant grandement personnalisable. Tout peut être réalisé avec la commande {config}.\n\n**Accessibilité**\nVous pouvez changer la langue avec {language}.\nPour nous aider à traduire, rejoignez nous sur {translateSite}.\n\nSi tout cela devient trop compliqué pour vous, rejoignez le [Serveur Support]({support}) et posez vos questions à la communauté dans le salon Support.',
     footer: 'En ligne depuis {time}',
     button: {
         learnmore: 'En savoir plus',
