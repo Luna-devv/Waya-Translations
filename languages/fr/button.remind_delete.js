47c7a817f4e04e0a9b180b83445df88e901924af
Luna <luni.siaa@gmail.com>
2023-01-29 11:51:40 +0100

    New translations button.remind_delete.js (French)

--- languages/fr/button.remind_delete.js
+++ languages/fr/button.remind_delete.js
@@ -1,4 +1,4 @@
 module.exports = {
-    cantFindId: 'I cannot find any active reminders with the ID `{id}`',
-    deleted: 'I\'ve deleted your reminder with the ID `{id}`'
+    cantFindId: 'Je ne trouve aucun rappel avec l\'ID `{id}`',
+    deleted: 'J\'ai supprimé votre rappel avec l\'ID `{id}`'
 };
