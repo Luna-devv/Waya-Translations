c8400ab24329dff963d64613e2683859844a35ab
Luna <luni.siaa@gmail.com>
2023-01-29 19:34:22 +0100

    New translations interaction.prompt.js (Austrian)

--- languages/de/interaction.prompt.js
+++ languages/de/interaction.prompt.js
@@ -1,29 +1,29 @@
 module.exports = {
-    validate: 'Validiere Daten...',
-    create: 'Erstelle {type} Prompt...',
-    tooManyRoles: 'Du hast {roles} rollen auf deinem Server, Bitte stelle sicher, dass du weniger als 244 hast!',
-    created: 'Die {type} Prompt wurde erstellt.',
+    validate: 'Validate data...',
+    create: 'Creating {type} prompt...',
+    tooManyRoles: 'You have {roles} roles in your server, please make sure that you have less than 244!',
+    created: 'The {type} prompt has been created.',
     type: {
-        category: 'kategorie',
-        support: 'support rolle',
-        emote: 'Benutzerdefiniertes Emoji',
-        endPoll: 'abstimmung enden'
+        category: 'channel category',
+        support: 'support role',
+        emote: 'custom emote',
+        endPoll: 'end poll'
     },
     message: {
-        created: 'erstellt',
-        noSupport: 'keine (benutze **/config**)',
-        invalidEmote: 'nicht im selben Server',
-        content: 'Inhalt',
-        endPoll: 'benutzte {command}'
+        created: 'created',
+        noSupport: 'none (use **/config**)',
+        invalidEmote: 'not in same guild',
+        content: 'Content',
+        endPoll: 'use {command}'
     },
     error: {
-        tooLong: 'Option {index} hat mehr als {number} Buchstaben!',
-        noData: 'keine daten angegeben'
+        tooLong: 'Option {index} has more than {number} characters!',
+        noData: 'no data set'
     },
-    verifySetUp: '**CAPTCHA Setup ist erforderlich**\nBitte benutze {command} und gehe zu *\'Logging > Joins > CAPTCHA\'* und führe diesen Befehl erneut.',
+    verifySetUp: '**CAPTCHA Setup is Required**\nPlease use {command} and go to *\'Logging > Joins > CAPTCHA\'* and run this command again.',
     webedit: {
-        title: 'Nachricht Web Editor',
-        description: 'Öffne [diese Website]({url}), um die Nachricht zu bearbeiten und einzubetten.',
-        open: 'Web Editor öffnen'
+        title: 'Message Web Edit',
+        description: 'Open [this Website]({url}) to start editing the message & embed.',
+        open: 'Open Web Edit'
     }
 };
