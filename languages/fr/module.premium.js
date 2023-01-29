83390ae405840acdc501bc7c46705916dc013875
Luna <luni.siaa@gmail.com>
2023-01-29 16:48:46 +0100

    New translations module.premium.js (French)

--- languages/fr/module.premium.js
+++ languages/fr/module.premium.js
@@ -1,22 +1,22 @@
 module.exports = {
     end: {
-        title: 'Subscription End',
-        description: 'Hey {user}, we have just recieved your subscription cancelation.\nWe\'re sad yo see you go, but we hope that you enjoyed it! {emote}',
+        title: 'Fin de votre abonnement',
+        description: 'Hey {user}, nous venons de recevoir votre annulation d\'abonnement.\nNous sommes tristes de vous voir partir, mais nous espérons que vous avez apprécié ! {emote}',
         notes: {
-            title: 'Please Note:',
-            support: 'If you need help, plese [Contact Support]({support})',
-            reSubscribe: 'If you want to reSubscribe, go to {url}'
+            title: 'Quelques détails :',
+            support: 'Si vous avez besoin d\'aide, [contactez le support]({support})',
+            reSubscribe: 'Si vous voulez vous réabonner, rendez-vous sur {url}'
         }
     },
     new: {
-        title: 'New Enterprise Subscription',
-        description: 'Hey {user}, we have just recieved your successfull payment. {emote}\nWe are really happy to see you on board, and we hope that you will like using the bot with your new benefits!',
+        title: 'Nouvel abonnement Entreprise',
+        description: 'Hey {user}, nous venons de recevoir votre paiement. {emote}\nNous sommes heureux de vous avoir à nos côtés, et nous espérons que vous apprécierez utiliser le bot avec vos nouveaux avantages !',
         notes: {
-            title: 'Please Note:',
-            setGuild: 'To set a Enterprise guild, please use {command}',
-            getGuild: 'If you need help with [getting your Server\'s Id]({article})',
-            cooldown: 'Don\'t forget that you always have a {days} days cooldown!',
-            support: 'If you need help, plese [Contact Support]({support})'
+            title: 'Quelques détails :',
+            setGuild: 'Pour appliquer votre abonnement à un serveur, veuillez utiliser {command}',
+            getGuild: 'Si vous avez besoin d\'aide avec [la récupération de l\'ID de votre serveur]({article})',
+            cooldown: 'N\'oubliez pas que vous ne pouvez changer de serveur que tous les {days} jours !',
+            support: 'Si vous avez besoin d\'aide, [contactez le support]({support})'
         }
     }
 };
