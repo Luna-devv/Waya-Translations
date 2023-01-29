4815bc9c90f233daa7bccc11ad06f36a9aed8366
Luna <luni.siaa@gmail.com>
2023-01-29 20:37:48 +0100

    New translations command.premium.js (Albanian)

--- languages/sq/command.premium.js
+++ languages/sq/command.premium.js
@@ -1,10 +1,10 @@
 module.exports = {
     set: {
-        noGuild: 'Please enter the Guild id of the Server you want to add Enterprise. [How to get the Id?]({article})',
-        noSubscription: 'You are currently not an Enterprise Subscriber, if you want to have access to special benefits and want to support the development, consider [buying Enterprise]({url}).',
-        onCooldown: 'You are currently on cooldown, note that you can only change the guild all {days} days, {remaining}.',
-        notFound: 'The selected Guild could not be found, please be sure that I am in this Server!',
-        noPerms: '{guild} is not owned by you nor do you manage it!',
+        noGuild: 'Ju lutemi shkruani Id-në e Guild-it të serverit që dëshironi të shtoni Enterprise. [Si të merrni Id-në?]({article})',
+        noSubscription: 'Aktualisht nuk jeni abonent i ndërmarrjes, nëse dëshironi të keni akses në përfitime të veçanta dhe dëshironi të mbështesni zhvillimin, merrni parasysh [blerjen e Abonimit]({url}).',
+        onCooldown: 'Jeni aktualisht në freskim, kini parasysh se mund ta ndryshoni repartin vetëm gjatë gjithë {days} ditëve, {remaining}.',
+        notFound: 'Serveri i përzgjedhur nuk u gjet, ju lutemi sigurohuni që unë jam në këtë server!',
+        noPerms: '{guild} nuk është në pronësi të ju dhe as nuk e menaxhoni atë!',
         alreadyHas: '{guild} already seems like to have Enterprise enabled!',
         success: 'WooHoo, {user}! You have successfully activated Enterprise in {guild}!'
     }
