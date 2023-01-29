15b3824045844969de1c3f75ad0687c101be0e79
Luna <luni.siaa@gmail.com>
2023-01-29 11:52:02 +0100

    New translations module.vote.js (uwu English)

--- languages/uw/module.vote.js
+++ languages/uw/module.vote.js
@@ -2,7 +2,7 @@ module.exports = {
     greeting: 'UwU thewe, {user} >~<',
     description: {
         notice: 'Thanks fow voting, it weawwy hewps us {emote}\nIf u want to continyue suppowting this pwoject by voting on top.gg, considew enyabwing the vote wemindew by using {command}! Thank u :)',
-        notice_disclaimer: 'u onwy wecieve this message once',
+        notice_disclaimer: 'u onwy weceive this message once',
         remind: 'U r nyow abwe to vote again fow me on top.gg! ผ(•̀_•́ผ)',
         data: 'U ve **wast voted {relativeTime}** and in totaw {amount} times.',
         new: '**NyEW!** Awso suppowt us on {url} with ur vote! （〃｀ 3′〃）'
