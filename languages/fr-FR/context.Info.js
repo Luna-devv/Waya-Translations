0b5b6625d5637d18843102438853e0255626e8e5
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:30 +0100

    New translations context.Info.js (French)

---
+++ languages/fr-FR/context.Info.js
@@ -0,0 +1,41 @@
+module.exports = {
+    messages: 'Messages',
+    name: 'Nom',
+    created: 'Créé',
+    joined: 'Rejoint',
+    invites: 'Invitations',
+    voice: 'Vocal',
+    none: 'aucun',
+    timeout: 'Exclu pour {time}',
+    pronouns: {
+        title: 'Pronoms',
+        none: 'Aucun pronom défini'
+    },
+    button: {
+        guildAvatar: 'URL de l\'avatar spécifique à ce serveur',
+        avatar: 'URL de l\'avatar',
+        banner: 'URL de la bannière'
+    },
+    actions: {
+        notAllowed: 'Vous ne pouvez pas utiliser ce menu.',
+        invalidTime: 'La durée doit être supérieure à 1 seconde !',
+        otherInvalidTime: 'La durée doit être inférieure à 28 jours !',
+        placeholder: 'Choisissez une action',
+        option: {
+            ban: 'Bannir l\'utilisateur',
+            kick: 'Expulser l\'utilisateur',
+            timeout: 'Exclure l\'utilisateur',
+            role: 'Assigner un rôle'
+        },
+        form: {
+            reason: 'Raison',
+            note: 'Note du modérateur',
+            duration: 'Durée'
+        },
+        success: {
+            ban: '{user} a bien été banni du serveur {guild}.',
+            kick: '{user} a bien été expulsé du serveur {guild}.',
+            timeout: '{user} a bien été exclu du serveur {guild}.'
+        }
+    }
+};
