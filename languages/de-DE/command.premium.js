6658c169e674d51597517d7792a69885d38e661c
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:44 +0100

    New translations command.premium.js (German)

---
+++ languages/de-DE/command.premium.js
@@ -0,0 +1,11 @@
+module.exports = {
+    set: {
+        noGuild: 'Bitte gib die Server Id des Servers ein, den du Enterprise hinzufügen möchtest. [Wie erhalte ich die Id?]({article})',
+        noSubscription: 'Du bist derzeit kein Enterprise Kunde, wenn du Zugang zu besonderen Vorteilen haben und die Entwicklung unterstützen möchtest, solltest du [Enterprise kaufen]({url}) in Betracht ziehen.',
+        onCooldown: 'Du bist derzeit im Cooldown, beachte, dass du den Server nur alle {days} Tage ändern kannst, {remaining}.',
+        notFound: 'Der ausgewählte Server konnte nicht gefunden werden, überprüfe ob ich in diesem Server bin!',
+        noPerms: '{guild} gehört dir nicht und wird von dir auch nicht verwaltet!',
+        alreadyHas: '{guild} scheint bereits Enterprise aktiviert zu haben!',
+        success: 'WooHoo, {user}! Du hast erfolgreich Enterprise in {guild} aktiviert!'
+    }
+};
