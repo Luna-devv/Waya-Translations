6ac396f7635f9ba5b6cbe41135ea1716ec357601
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:50 +0100

    New translations module.premium.js (Italian)

---
+++ languages/it-IT/module.premium.js
@@ -0,0 +1,22 @@
+module.exports = {
+    end: {
+        title: 'Abbonamento Terminato',
+        description: 'Ciao {user}, abbiamo appena ricevuto la cancellazione del tuo abbonamento.\nCi dispiace che tu te ne vai, ma speriamo che ti sia piacuto! {emote}',
+        notes: {
+            title: 'Nota che:',
+            support: 'Se hai bisogno di aiuto [Contatta l\'assistenza]({support})',
+            reSubscribe: 'Se vuoi riabbonarti, vai a {url}'
+        }
+    },
+    new: {
+        title: 'Nuovo Abbonamento',
+        description: 'Ciao {user}, abbiamo appena ricevuto il tuo pagamento con successo. {emote}\nSiamo davvero felici di vederti a bordo, e speriamo che ti piacerà usare il bot con i tuoi nuovi vantaggi!',
+        notes: {
+            title: 'Nota bene:',
+            setGuild: 'Per impostare una Gilda con Abbonamento, utilizza {command}',
+            getGuild: 'Se hai bisogno di aiuto per [ottenere l\'Id del tuo server]({article})',
+            cooldown: 'Non dimenticare che hai sempre {days} giorni di raffreddamento!',
+            support: 'Se hai bisogno di aiuto [Contatta l\'assistenza]({support})'
+        }
+    }
+};
