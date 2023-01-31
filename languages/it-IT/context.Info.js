ed3a2ae6badbca71becfe4707e4d533bde1b04af
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:32 +0100

    New translations context.Info.js (Italian)

---
+++ languages/it-IT/context.Info.js
@@ -0,0 +1,41 @@
+module.exports = {
+    messages: 'Messaggi',
+    name: 'Nome',
+    created: 'Creato',
+    joined: 'Unito',
+    invites: 'Inviti',
+    voice: 'Canale vocale',
+    none: 'nessuno',
+    timeout: 'in Timeout per {time}',
+    pronouns: {
+        title: 'Pronomi',
+        none: 'Nessun pronome impostato'
+    },
+    button: {
+        guildAvatar: 'URL Avatar per Gilda',
+        avatar: 'URL dell\'avatar',
+        banner: 'URL dell\'banner'
+    },
+    actions: {
+        notAllowed: 'You can\'t use this menu.',
+        invalidTime: 'La durata non può essere meno di un secondo!',
+        otherInvalidTime: 'La durata non può essere più di 28 giorni!',
+        placeholder: 'Scegli un\'azione',
+        option: {
+            ban: 'Bandisci Utente',
+            kick: 'Espelli Utente',
+            timeout: 'Timeout Utente',
+            role: 'Assegna ruolo'
+        },
+        form: {
+            reason: 'Motivo',
+            note: 'Nota moderatore',
+            duration: 'Durata'
+        },
+        success: {
+            ban: 'Bannato {user} da {guild} con successo.',
+            kick: 'Espulso {user} da {guild} von successo.',
+            timeout: 'Messo {user} in timeout in {guild} con successo.'
+        }
+    }
+};
