13dfda10bfc207a795d69d21e72be5af3c34a64d
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:03 +0100

    New translations interaction.prompt.js (Croatian)

---
+++ languages/hr-HR/interaction.prompt.js
@@ -0,0 +1,29 @@
+module.exports = {
+    validate: 'Potvrđivanje podataka...',
+    create: 'Izrada {type} upita...',
+    tooManyRoles: 'Imate {roles} uloga na svom serveru, provjerite imate li manje od 244!',
+    created: 'Prompt {type} je stvoren.',
+    type: {
+        category: 'kategorija kanala',
+        support: 'uloga podrške',
+        emote: 'prilagođeni emote',
+        endPoll: 'kraj ankete'
+    },
+    message: {
+        created: 'napravljen',
+        noSupport: 'prazno (koristite **/config**)',
+        invalidEmote: 'nije u istom serveru',
+        content: 'Sadržaj',
+        endPoll: 'koristi {command}'
+    },
+    error: {
+        tooLong: 'Opcija {index} ima više od {number} znakova!',
+        noData: 'nema skupa podataka'
+    },
+    verifySetUp: '**Potrebno je postavljanje CAPTCHA**\nUpotrijebite {command} i idite na *\'Zapisivanje > Pridruživanja > CAPTCHA\'* i ponovno pokrenite ovu naredbu.',
+    webedit: {
+        title: 'Poruka Web Edit',
+        description: 'Otvorite [ovu web-stranicu]({url}) za početak uređivanja poruke i ugradite je.',
+        open: 'Otvorite Web Edit'
+    }
+};
