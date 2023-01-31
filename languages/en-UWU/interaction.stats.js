dd117b29973a9fb81e5efe81558562756da60666
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:18 +0100

    New translations interaction.stats.js (UwU English)

---
+++ languages/en-UWU/interaction.stats.js
@@ -0,0 +1,22 @@
+module.exports = {
+    fetchError: `Couwd nyot fetch data, pwease twy again watew!`,
+    links: {
+        website: 'website',
+        invite: 'invite',
+        more: 'in-depth stats'
+    },
+    general: {
+        title: 'Genyewaw',
+        uptime: '**{time}** uptime',
+        guilds: '**{count}** guiwds',
+        users: '**{count}** usews',
+        invites: '**{count}** invites',
+        starboards: '**{count}** stawboawds',
+        executed: '**{count}** executed',
+        usage: '{cpu}% cpu • {mem}% memowy'
+    },
+    commandList: {
+        title: 'Popuwaw',
+        none: 'nyonye'
+    }
+};
