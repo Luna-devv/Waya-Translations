81884f4ff5986bb11c0c35a54cf1930c9aa147b6
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:09 +0100

    New translations interaction.remind.js (UwU English)

---
+++ languages/en-UWU/interaction.remind.js
@@ -0,0 +1,24 @@
+module.exports = {
+    error: {
+        tooShort: 'The time cannyot be wess than >.< **1 second**.. uwu  ',
+        tooLong: 'The time cannyot be mowe than **24 days**.. >.<',
+        cantFindId: 'I cannyot find any active wemindews with the ID `{id}`, Sowwy babe >~<'
+    },
+    reminder: 'UwU {user} >~<, hewe\'s ur wemindew.',
+    remindSet: {
+        created: 'I wiww wemind u in **{time}**!',
+        id: 'Id'
+    },
+    remindView: {
+        created: 'Cweated',
+        remindsIn: 'Weminds',
+        message: 'Message',
+        button: {
+            delete: 'Dewete',
+            invite: 'Invite {user}'
+        },
+        reminderCount: 'U have {count} wemindews',
+        createFirst: 'Use {command} fiwst to cweate a wemindew!'
+    },
+    remindDelete: { deleted: 'I\'ve deweted ur wemindew with the Id `{id}` qwq' }
+};
