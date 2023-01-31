63f5555d7186d3e5f590401e1e1175759a52c79e
Luna <luni.siaa@gmail.com>
2023-01-31 08:34:01 +0100

    New translations command.snipe.js (Albanian)

---
+++ languages/sq-AL/command.snipe.js
@@ -0,0 +1,9 @@
+module.exports = {
+    none: 'Nuk kishte asnjë mesazh të Fshirë/Redaktuar në {time} e fundit.',
+    header: 'Nga {user} në {channel}:',
+    disclaimer: 'Snajpat më të fundit {amount} në {time} e fundit',
+    type: {
+        edit: 'Redakto:',
+        delete: 'Fshij:'
+    }
+};
