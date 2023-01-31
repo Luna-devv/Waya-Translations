b217f97f6a08138823800de9ab9da2b0e472a7c6
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:26 +0100

    New translations context.ForceStarboard.js (German)

---
+++ languages/de-DE/context.ForceStarboard.js
@@ -0,0 +1,12 @@
+module.exports = {
+    fail: {
+        config: 'Das Starboard ist in diesem Server nicht eingerichtet..',
+        content: 'Die Nachricht hat keinen Inhalt Wedernoch Anhänge..',
+        channel: 'Der eingerichtete Starboard Channel exestiert nicht mehr..',
+        exists: {
+            text: 'Die Nachricht ist bereits im Starboard..',
+            button: 'Zur Nachricht'
+        }
+    },
+    success: 'Die Nachricht ist nun im Starboard.'
+};
