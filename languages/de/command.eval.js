ff82fbc74b6d82479cccbca3cca456dc98b5c9ac
Luna <luni.siaa@gmail.com>
2023-01-31 08:22:50 +0100

    New translations command.eval.js (Austrian)

--- languages/de/command.eval.js
+++ languages/de/command.eval.js
@@ -1,4 +1,4 @@
 module.exports = {
-    result: 'ergebnis',
-    tooLong: 'Das Ergebnis ist zu lang.. Die Evaluierung ist in der angehängten Datei zu sehen.'
+    result: 'result',
+    tooLong: 'The result is too long.. The result can be seen in the attached file.'
 };
