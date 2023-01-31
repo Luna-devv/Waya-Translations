f6ea0359c4c9e051505f1bb60aaba9b88703ccef
Luna <luni.siaa@gmail.com>
2023-01-31 08:31:49 +0100

    New translations button.ticket_close.js (Croatian)

---
+++ languages/hr-HR/button.ticket_close.js
@@ -0,0 +1,17 @@
+module.exports = {
+    request: { ureStaff: 'Vi ste osoblje, možete samo koristiti {command}.' },
+    noData: 'Ne mogu pronaći server u bazi podataka!',
+    confirm: {
+        text: 'Jeste li sigurni da želite zauvijek izbrisati {channel}?',
+        configReason: 'Za `razlog`, molimo omogućite prijavu u `/config` => __Ulaznice__!',
+        required: 'Potreban je razlog za zatvaranje!',
+        button: {
+            close: 'Zatvori ulaznicu',
+            closeReason: 'Zatvorite ulaznicu s razlogom'
+        }
+    },
+    form: {
+        formTitle: `Zatvori {channel}`,
+        reason: 'Razlog'
+    }
+};
