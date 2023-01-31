e5f36d767648603058a6a6c93e9812cd1c3aa2da
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:45 +0100

    New translations command.premium.js (Italian)

---
+++ languages/it-IT/command.premium.js
@@ -0,0 +1,11 @@
+module.exports = {
+    set: {
+        noGuild: 'Inserisci l\'id della Gilda del Server che vuoi aggiungere al Abbonamento. [Come ottenere l\'Id?]({article})',
+        noSubscription: 'Al momento non sei un abbonato, se vuoi avere accesso a benefici speciali e vuoi supportare lo sviluppo, prendi in considerazione a [comprare l\'Abbonamento]({url}).',
+        onCooldown: 'Al momento sei in fase di raffreddamento, nota che puoi cambiare la gilda solo tutti i {days} giorni, {remaining}.',
+        notFound: 'La Gilda selezionata non può essere trovata, assicurati che ci sono in questo Server!',
+        noPerms: '{guild} non è di tua proprietà né lo gestisci!',
+        alreadyHas: '{guild} sembra già avere un Abbonamento abilitato!',
+        success: 'WooHoo, {user}! Hai attivato l\'Abbonamento in {guild} von successo!'
+    }
+};
