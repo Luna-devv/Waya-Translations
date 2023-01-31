f3ffdd9b3458cb0a71b180d927af97cee873bf75
Luna <luni.siaa@gmail.com>
2023-01-31 08:34:10 +0100

    New translations module.verification.js (German)

---
+++ languages/de-DE/module.verification.js
@@ -0,0 +1,21 @@
+module.exports = {
+    join: {
+        title: 'Willkommen, {user}!',
+        description: 'Um Zugang zu diesem Server zu erhalten, mustt du eine schnelle Verifikation abschließen, um das Risiko von Bot-Attacken zu senken und um die Überholung der Sicherheit zu gewährleisten {emote}\n\nBitte beachte, dass die Admins und Mods von {guild} keinen Zugriff auf die von dir bereitgestellten Informationen haben.',
+        premium: 'Danke, dass du ein Enterprise-Mitglied bist und dieses Projekt unterstützt {emote}\n\nBeachten, dass du die Verifizierung in {guild} nicht abschließen musst, da das Überspringen der Verifizierung Teil deines Enterprise-Plans ist.'
+    },
+    punishment: {
+        ban: 'Gebannt von {guild}',
+        kick: 'Gekicket from {guild}',
+        timeout: 'Gemuted in {guild}',
+        role: 'Rolle zugewiesen in {guild}'
+    },
+    reason: {
+        filter: 'Account ist jünger als {days} Tage ({relative})',
+        captcha: 'Fehlgeschlagene Verifizierung (CAPTCHA)'
+    },
+    button: {
+        verify: 'Verifikation abschließen',
+        privacy: 'Datenschutzerklärung'
+    }
+};
