c33e624c0f44731d282c2e9103aa2a33ac8d0ca5
Luna <luni.siaa@gmail.com>
2023-01-31 08:34:15 +0100

    New translations command._ticket.js (Italian)

---
+++ languages/it-IT/command._ticket.js
@@ -0,0 +1,9 @@
+module.exports = {
+    noMember: 'Si prega di fornire un membro valido come argomento.',
+    add: { success: '{user} è stato aggiunto con successo a questo Ticket!' },
+    remove: { success: '{user} è stato rimosso con successo da questo Ticket!' },
+    claim: {
+        already: ';[{user} è, sei] già gestendo questo ticket!',
+        success: 'Hai trasferito il ticket a {user} con successo!'
+    }
+};
