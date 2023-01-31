bae202370ca173d7f6614a93c662aced83fa4fdf
Luna <luni.siaa@gmail.com>
2023-01-31 08:20:54 +0100

    New translations command.tags.js (German)

--- languages/de/command.tags.js
+++ languages/de/command.tags.js
@@ -3,13 +3,13 @@ module.exports = {
         text: 'Text ;[Command, Commands]',
         slash: 'Slash ;[Command, Commands]'
     },
-    none: 'no tags',
-    disabled: '{amount} ;[tag is, tags are] hidden due to missing permissions',
+    none: 'keine Tags',
+    disabled: '{amount} ;[Tag, Tags] sind versteckt aufgrund fehlender rechte',
     // ;[singular, plural]
-    amount: '{amount} tags • Use with {command}',
+    amount: '{amount} Tags • Mit {command} verwenden',
     errors: {
-        overLimit: 'You have reached the guild limit of **{count}/{max} tags**!\n\nNote that this tag exceeds the limits and is disabled until you increase the limit again.',
-        invalidTag: 'A Tag with this name doesn\'t exist!',
-        ticketUse: 'This Tag is only available in tickets using {user}.'
+        overLimit: 'Du hast das Serverlimit von **{count}/{max} Tags** erreicht!\n\nBitte beachte, dass dieser Tag die Limits überschreitet und deaktiviert ist, bis du das Limit wieder erhöhen.',
+        invalidTag: 'Es existiert kein Tag mit diesen Namen!',
+        ticketUse: 'Dieser Tag ist nur in Tickets, die {user} benutzen, verfügbar.'
     }
 };
