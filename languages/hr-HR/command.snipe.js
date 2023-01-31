f7565bbdc5603f79cfd7f74bb0cd96f9152cf8e9
Luna <luni.siaa@gmail.com>
2023-01-31 08:34:01 +0100

    New translations command.snipe.js (Croatian)

---
+++ languages/hr-HR/command.snipe.js
@@ -0,0 +1,9 @@
+module.exports = {
+    none: 'Nije bilo Izbrisanih/Uređenih poruka u zadnjih {time}.',
+    header: 'Od {user} u {channel}:',
+    disclaimer: 'Zadnjih {amount} provjera u zadnjih {time}',
+    type: {
+        edit: 'Uredi:',
+        delete: 'Izbrisati:'
+    }
+};
