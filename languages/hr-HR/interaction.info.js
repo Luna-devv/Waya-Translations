09a74413d1854368b44004ee993fa676120a8710
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:48 +0100

    New translations interaction.info.js (Croatian)

---
+++ languages/hr-HR/interaction.info.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Pozivnice korisnika {user}',
+    building: 'Izgrađivanje...',
+    more: '+ još {count}',
+    none: 'prazno',
+    button: {
+        ids: 'ID-ovi korisnika',
+        mentions: 'Spominjanja korisnika',
+        export: 'Izvezi u CSV'
+    },
+    export: 'Preuzmite .CVS u nastavku i uvezite ju u excel!'
+};
