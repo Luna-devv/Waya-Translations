26bee7fed04ade32e282f5f3d66dd25c8a70b3c6
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:29 +0100

    New translations module.pronouns.js (German)

---
+++ languages/de-DE/module.pronouns.js
@@ -0,0 +1,33 @@
+module.exports = {
+    disclaimer: 'Du musst eine einstellung auswählen, um diese Einstellung zu speichern',
+    choice: {
+        remove: {
+            question: 'Von wo willst du {prn} als Pronomen entfernen?',
+            button: {
+                global: 'Überall',
+                guild: 'Von diesem Server'
+            }
+        },
+        add: {
+            question: 'Wo willst du {prn} als Pronomen hinzufügen?',
+            button: {
+                global: 'Überall',
+                guild: 'In diesem Server'
+            }
+        }
+    },
+    success: {
+        removed: {
+            global: '{prn} wurde überall erfolgreich als Pronomen entfernt.',
+            guild: '{prn} wurde erfolgreich als Pronomen in diesem Server entfernt.'
+        },
+        added: {
+            global: '{prn} wurde überall erfolgreich als Pronomen hinzugefügt.',
+            guild: '{prn} wurde erfolgreich als Pronomen in diesem Server hinzugefügt.'
+        }
+    },
+    error: {
+        tooSlow: 'Du warst zu langsam, probiere es noch einmal!',
+        fail: 'Speichern fehlgeschlagen, bitte probiere es noch einmal!'
+    }
+};
