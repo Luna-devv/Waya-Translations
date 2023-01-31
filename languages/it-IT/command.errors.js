ce2a58e3c1b3aa002ced1b0f7b67f025c9199512
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:02 +0100

    New translations command.errors.js (Italian)

---
+++ languages/it-IT/command.errors.js
@@ -0,0 +1,13 @@
+module.exports = {
+    notFound: 'Questo problema, non è stato trovato',
+    view: {
+        main: 'a {time} durante l\'esecuzione di {command} per {user}',
+        fixed: 'Questo problema è stato risolto',
+        unfixed: 'Questo errore non è risolto'
+    },
+    check: {
+        fixed: 'Stato del problema aggiornato a risolto.',
+        unfixed: 'Stato del problema aggiornato a irrisolto.'
+    },
+    deleted: 'Il problema è stato eliminato.'
+};
