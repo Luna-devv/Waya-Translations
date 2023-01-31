27820d999b71a3a4f02539f2a5aaa1b70c5b5ea3
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:15 +0100

    New translations interaction.stats.js (German)

---
+++ languages/de-DE/interaction.stats.js
@@ -0,0 +1,22 @@
+module.exports = {
+    fetchError: `Daten konnten nicht gefetched werden, bitte versuche es später erneut!`,
+    links: {
+        website: 'webseite',
+        invite: 'einladen',
+        more: 'detaillierte Statistiken'
+    },
+    general: {
+        title: 'Allgemein',
+        uptime: '**{time}** Uptime',
+        guilds: '**{count}** servers',
+        users: '**{count}** users',
+        invites: '**{count}** einladungen',
+        starboards: '**{count}** starboards',
+        executed: '**{count}** commands',
+        usage: '{cpu}% cpu • {mem}% ram'
+    },
+    commandList: {
+        title: 'Beliebt',
+        none: 'keine'
+    }
+};
