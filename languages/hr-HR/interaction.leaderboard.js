d7bf6d46f3e763e785be71b909d82d3fcea6e404
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:58 +0100

    New translations interaction.leaderboard.js (Croatian)

---
+++ languages/hr-HR/interaction.leaderboard.js
@@ -0,0 +1,8 @@
+module.exports = {
+    messages: 'Ljestvica Poruka',
+    voice: 'Ljestvica Poziva',
+    invites: 'Ljestvica Pozivnica',
+    none: 'prazno',
+    personalized: 'Tvoja pozicija',
+    total: '{amount} ;[korisnici su, korisnik je] na ovoj ploči s najboljim rezultatima'
+};
