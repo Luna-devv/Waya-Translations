dbe359238160439fc879f69110d5396b68f318f5
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:46 +0100

    New translations interaction.info.js (Italian)

---
+++ languages/it-IT/interaction.info.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Inviti di {user}\'',
+    building: 'Costruzione...',
+    more: '+ {count} più',
+    none: 'nessuno',
+    button: {
+        ids: 'ID degli Utenti',
+        mentions: 'Menzioni Utente',
+        export: 'Esporta CSV'
+    },
+    export: 'Scarica il .CVS qui sotto e importalo in excel!'
+};
