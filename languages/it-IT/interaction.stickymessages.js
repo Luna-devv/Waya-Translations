87a0ff99c87b094af44142f4b0d77d2e85bc322b
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:55 +0100

    New translations interaction.stickymessages.js (Italian)

---
+++ languages/it-IT/interaction.stickymessages.js
@@ -0,0 +1,46 @@
+module.exports = {
+    alreadyExists: 'Un messaggio adesivo in questo canale esiste già!',
+    stickymessagesLimit: 'Hai raggiunto il limite di **{count}/{max} Messaggi Adesivi**!',
+    invalidStickymessage: 'Nessun messaggio adesivo esiste in questo canale!',
+    overview: {
+        title: 'Configurazione Messaggio Adesivo',
+        message: 'Messaggio',
+        embed: 'Embed',
+        note: 'Nota Bottone',
+        note_status: {
+            show: 'Mostrando',
+            hide: 'Nascosto'
+        },
+        button: {
+            exit: 'Salva ed esci',
+            color: {
+                enabled: 'Scegli un colore per l\'Embed',
+                disabled: 'Usa Embed e seleziona un colore'
+            },
+            note: {
+                show: 'Mostra nota',
+                hide: 'Nascondi nota'
+            }
+        }
+    },
+    form: {
+        formName: '{name} messaggio adesivo',
+        message: { content: 'Contenuto' },
+        embed: {
+            title: 'Titolo',
+            description: 'Descrizione',
+            thumbnail: 'Miniatura',
+            image: 'Immagine',
+            footer: 'Piè di Pagina'
+        }
+    },
+    success: {
+        message: 'Aggiunto Messaggio Adesivo in {channel} con successo.',
+        try: 'Provalo inviando alcuni messaggi lì!'
+    },
+    list: {
+        title: 'Adesivo ;[Messaggio, Messaggi]',
+        none: 'nessun messaggio adesivo'
+    },
+    deleted: 'Il messaggio adesivo in {channel} è stato eliminato con successo.'
+};
