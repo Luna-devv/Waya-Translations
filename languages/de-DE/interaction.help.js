1ce28696379cb405a6a380680eb386c0b024e82a
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:41 +0100

    New translations interaction.help.js (German)

---
+++ languages/de-DE/interaction.help.js
@@ -0,0 +1,8 @@
+module.exports = {
+    description: '**Über {name}**\nIch werde von {devs} entwickelt, icons by [Danu] (https://waya.one/go/danu_icons).\n{name} ist so konzipiert, dass es sehr einfach zu konfigurieren ist, aber gleichzeitig die besten Anpassungsmöglichkeiten bietet. Alles kann mit einem einfachen {config} Command erledigt werden.\n\n**Barrierefreiheit**\nWenn du die Sprache ändern willst, benutze bitte den {language} command.\nWenn du uns bei der Übersetzung helfen willst, trette uns auf {translateSite} bei.\n\nWenn du damit überfordert bist, Join einfach unseren [Support Server]({support}) und frag\' unsere Community im Support Channel um Hilfe.',
+    footer: 'Online seit {time}',
+    button: {
+        learnmore: 'Mehr Erfahren',
+        vote: 'Voten'
+    }
+};
