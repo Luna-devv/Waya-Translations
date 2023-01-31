6191ec0d4d8f90c01d5d69419b779f50df53e874
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:07 +0100

    New translations interaction.remind.js (Croatian)

---
+++ languages/hr-HR/interaction.remind.js
@@ -0,0 +1,24 @@
+module.exports = {
+    error: {
+        tooShort: 'Vrijeme ne može biti kraće od **1 sekunde**..',
+        tooLong: 'Vrijeme ne može biti duže od **24 dana**..',
+        cantFindId: 'Ne mogu pronaći aktivne podsjetnike s ID-om `{id}`'
+    },
+    reminder: 'Hej {user}, evo vašeg podsjetnika.',
+    remindSet: {
+        created: 'Podsjetit ću te za **{vrijeme}**!',
+        id: 'ID'
+    },
+    remindView: {
+        created: 'Napravljen',
+        remindsIn: 'Podsjeća',
+        message: 'Poruka',
+        button: {
+            delete: 'Izbrisati',
+            invite: 'Pozovi {user}'
+        },
+        reminderCount: 'Imate {count} podsjetnika',
+        createFirst: 'Prvo upotrijebite {command} za izradu podsjetnika!'
+    },
+    remindDelete: { deleted: 'Izbrisala sam vaš podsjetnik s ID-om `{id}`' }
+};
