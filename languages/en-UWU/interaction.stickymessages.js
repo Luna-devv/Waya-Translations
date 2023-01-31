d422d3cd00beb473734bee9ec9ba2267005e8d3c
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:58 +0100

    New translations interaction.stickymessages.js (UwU English)

---
+++ languages/en-UWU/interaction.stickymessages.js
@@ -0,0 +1,46 @@
+module.exports = {
+    alreadyExists: 'Baby, A Sticky Message in this channyew awweady exists! qwq',
+    stickymessagesLimit: 'Ooohhuu :( U have weached the guiwd wimit of **{count}/{max} Sticky Messages**! >~<',
+    invalidStickymessage: 'But babe.. Nyo Sticky Message exists in this channyew! :(',
+    overview: {
+        title: 'Sticky Message Config',
+        message: 'Message',
+        embed: 'Embed',
+        note: 'Button Nyote',
+        note_status: {
+            show: 'Showing',
+            hide: 'Hidden'
+        },
+        button: {
+            exit: 'Save & Exit',
+            color: {
+                enabled: 'Sewect a cowow fow the Embed',
+                disabled: 'Use Embeds and sewect a cowow'
+            },
+            note: {
+                show: 'Show Nyote',
+                hide: 'Hide Nyote'
+            }
+        }
+    },
+    form: {
+        formName: '{name} sticky message',
+        message: { content: 'Content' },
+        embed: {
+            title: 'Titwe',
+            description: 'Descwiption',
+            thumbnail: 'Thumbnyaiw',
+            image: 'Image',
+            footer: 'Footew'
+        }
+    },
+    success: {
+        message: 'Successfuwwy added a Sticky Message in {channel} >.>',
+        try: 'Ayy, twy it out by sending a few messages thewe :>'
+    },
+    list: {
+        title: 'Sticky ;[Message, Messages]',
+        none: 'nyo sticky messages'
+    },
+    deleted: 'Successfuwwy deweted the Sticky Message in {channel}. ^^'
+};
