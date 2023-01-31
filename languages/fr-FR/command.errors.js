1fbd188bd334de5bc4e65b295658e0b2f11bcbfb
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:01 +0100

    New translations command.errors.js (French)

---
+++ languages/fr-FR/command.errors.js
@@ -0,0 +1,13 @@
+module.exports = {
+    notFound: 'Cette erreur est introuvable',
+    view: {
+        main: 'à {time} lors de l\'exécution de {command} pour {user}',
+        fixed: 'Cette erreur a été corrigée',
+        unfixed: 'Cette erreur n\'est pas résolue'
+    },
+    check: {
+        fixed: 'État du problème défini à résolu.',
+        unfixed: 'État du problème défini à non résolu.'
+    },
+    deleted: 'Ce problème a été supprimé.'
+};
