659875a5f5f1f0f1b4a11f2b49d89c9fc88ae882
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:25 +0100

    New translations module.formatTime.js (Italian)

---
+++ languages/it-IT/module.formatTime.js
@@ -0,0 +1,8 @@
+// ;[singular, plural]
+module.exports = {
+    months: ';[mese, mesi]',
+    days: ';[giorno, giorni]',
+    hours: ';[ore, ore]',
+    minutes: ';[minuto, minuti]',
+    seconds: ';[secondo, secondi]'
+};
