e68bfc01e28f4ea4d6f77440872dfa3d931e91fa
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:50 +0100

    New translations interaction.info.js (UwU English)

---
+++ languages/en-UWU/interaction.info.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: '{user}\'s Invites',
+    building: 'Buiwding...',
+    more: '+ {count} mowe',
+    none: 'nyonye',
+    button: {
+        ids: 'Usew IDs',
+        mentions: 'Usew Mentions',
+        export: 'Expowt CSV'
+    },
+    export: 'u can ownwoad the .CVS bewow and impowt it into UWU excew! but might be a viwus >.<'
+};
