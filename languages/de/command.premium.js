18f10f11219c870614ca8530dd0cb5259477e5fd
Luna <luni.siaa@gmail.com>
2023-01-31 08:29:16 +0100

    New translations command.premium.js (German, Austria)

--- languages/de/command.premium.js
+++ languages/de/command.premium.js
@@ -1,11 +1,11 @@
 module.exports = {
     set: {
-        noGuild: 'Bitte gib die Server Id des Servers ein, den du Enterprise hinzufügen möchtest. [Wie erhalte ich die Id?]({article})',
-        noSubscription: 'Du bist derzeit kein Enterprise Kunde, wenn du Zugang zu besonderen Vorteilen haben und die Entwicklung unterstützen möchtest, solltest du [Enterprise kaufen]({url}) in Betracht ziehen.',
-        onCooldown: 'Du bist derzeit im Cooldown, beachte, dass du den Server nur alle {days} Tage ändern kannst, {remaining}.',
-        notFound: 'Der ausgewählte Server konnte nicht gefunden werden, überprüfe ob ich in diesem Server bin!',
-        noPerms: '{guild} gehört dir nicht und wird von dir auch nicht verwaltet!',
-        alreadyHas: '{guild} scheint bereits Enterprise aktiviert zu haben!',
-        success: 'WooHoo, {user}! Du hast erfolgreich Enterprise in {guild} aktiviert!'
+        noGuild: 'Please enter the Guild id of the Server you want to add Enterprise. [How to get the Id?]({article})',
+        noSubscription: 'You are currently not an Enterprise Subscriber, if you want to have access to special benefits and want to support the development, consider [buying Enterprise]({url}).',
+        onCooldown: 'You are currently on cooldown, note that you can only change the guild all {days} days, {remaining}.',
+        notFound: 'The selected Guild could not be found, please be sure that I am in this Server!',
+        noPerms: '{guild} is not owned by you nor do you manage it!',
+        alreadyHas: '{guild} already seems like to have Enterprise enabled!',
+        success: 'WooHoo, {user}! You have successfully activated Enterprise in {guild}!'
     }
 };
