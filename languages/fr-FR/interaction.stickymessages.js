91e2e4207e1569684c93ca5e04427ac6b4ab7a1a
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:54 +0100

    New translations interaction.stickymessages.js (French)

---
+++ languages/fr-FR/interaction.stickymessages.js
@@ -0,0 +1,46 @@
+module.exports = {
+    alreadyExists: 'Un message persistant existe déjà dans ce salon !',
+    stickymessagesLimit: 'Vous avez atteint la limite par serveur de **{count}/{max} messages persistants** !',
+    invalidStickymessage: 'Aucun message persistant n\'existe dans ce salon !',
+    overview: {
+        title: 'Configuration d\'un message persistant',
+        message: 'Message',
+        embed: 'Embed',
+        note: 'Note du bouton',
+        note_status: {
+            show: 'Affiché',
+            hide: 'Caché'
+        },
+        button: {
+            exit: 'Enregistrer et quitter',
+            color: {
+                enabled: 'Veuillez sélectionner une couleur pour l\'embed',
+                disabled: 'Utiliser un embed et sélectionner une couleur'
+            },
+            note: {
+                show: 'Afficher la note',
+                hide: 'Cacher la note'
+            }
+        }
+    },
+    form: {
+        formName: '{name} message persistant',
+        message: { content: 'Contenu' },
+        embed: {
+            title: 'Titre',
+            description: 'Description',
+            thumbnail: 'Icône',
+            image: 'Image',
+            footer: 'Texte de bas de page'
+        }
+    },
+    success: {
+        message: 'Votre message persistant a bien été ajouté dans {channel}.',
+        try: 'Essayez en envoyant quelques messages ici !'
+    },
+    list: {
+        title: ';[message persistant, messages persistants]',
+        none: 'pas de messages persistants'
+    },
+    deleted: 'Votre message persistant a bien été supprimé dans {channel}.'
+};
