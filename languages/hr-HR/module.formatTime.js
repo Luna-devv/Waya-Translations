95d5a2bfa19078119d83b27bf475edd37fa465eb
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:27 +0100

    New translations module.formatTime.js (Croatian)

---
+++ languages/hr-HR/module.formatTime.js
@@ -0,0 +1,8 @@
+// ;[singular, plural]
+module.exports = {
+    months: ':[mjesec, mjeseca]',
+    days: ';[dan, dana]',
+    hours: ';[sat, sata]',
+    minutes: ';[minuta, minute]',
+    seconds: ';[sekunda, sekunde]'
+};
