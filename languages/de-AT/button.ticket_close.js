16bcab73921726269e835770aa624af4f437499b
Luna <luni.siaa@gmail.com>
2023-01-31 08:31:49 +0100

    New translations button.ticket_close.js (German, Austria)

---
+++ languages/de-AT/button.ticket_close.js
@@ -0,0 +1,17 @@
+module.exports = {
+    request: { ureStaff: 'You are staff, you can just use {command}.' },
+    noData: 'Could not find guild in the Database!',
+    confirm: {
+        text: 'Are you sure that you want to delete {channel} forever?',
+        configReason: 'For `reason`, please enable logging in `/config` => __Ticketing__!',
+        required: 'A close reason is required!',
+        button: {
+            close: 'Close Ticket',
+            closeReason: 'Close Ticket with Reason'
+        }
+    },
+    form: {
+        formTitle: `Close {channel}`,
+        reason: 'Reason'
+    }
+};
