6880eeedf7ed16810790fbe9584359e8d7eb361e
Luna <luni.siaa@gmail.com>
2023-01-31 08:28:58 +0100

    New translations command.errors.js (German, Austria)

--- languages/de/command.errors.js
+++ languages/de/command.errors.js
@@ -1,13 +1,13 @@
 module.exports = {
-    notFound: 'Dieser Error konnte nicht gefunden werden',
+    notFound: 'This issue could not be found',
     view: {
-        main: 'um {time} während der Ausführung von {command} für {user}',
-        fixed: 'Dieser Error wurde behoben',
-        unfixed: 'Dieser Error wurde nicht behoben'
+        main: 'at {time} while executing {command} for {user}',
+        fixed: 'This error has been fixed',
+        unfixed: 'This error is unresolved'
     },
     check: {
-        fixed: 'Aktualisierte Error Status zu gelöst.',
-        unfixed: 'Aktualisierte Error Status zu ungelöst.'
+        fixed: 'Updated issue state to resolved.',
+        unfixed: 'Updated issue state to unresolved.'
     },
-    deleted: 'Dieser Error wurde nun gelöscht.'
+    deleted: 'This issue has been deleted.'
 };
