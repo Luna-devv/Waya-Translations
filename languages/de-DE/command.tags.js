1318a1a21d96427ded4c7b8a321db970c8e79871
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:16 +0100

    New translations command.tags.js (German)

---
+++ languages/de-DE/command.tags.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: {
+        text: 'Text ;[Command, Commands]',
+        slash: 'Slash ;[Command, Commands]'
+    },
+    none: 'keine Tags',
+    disabled: '{amount} ;[Tag, Tags] sind versteckt aufgrund fehlender rechte',
+    // ;[singular, plural]
+    amount: '{amount} Tags • Mit {command} verwenden',
+    errors: {
+        overLimit: 'Du hast das Serverlimit von **{count}/{max} Tags** erreicht!\n\nBitte beachte, dass dieser Tag die Limits überschreitet und deaktiviert ist, bis du das Limit wieder erhöhen.',
+        invalidTag: 'Es existiert kein Tag mit diesen Namen!',
+        ticketUse: 'Dieser Tag ist nur in Tickets, die {user} benutzen, verfügbar.'
+    }
+};
