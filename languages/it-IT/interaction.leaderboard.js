422a22c44bf80f095f5ac4a524134fc67d3548eb
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:56 +0100

    New translations interaction.leaderboard.js (Italian)

---
+++ languages/it-IT/interaction.leaderboard.js
@@ -0,0 +1,8 @@
+module.exports = {
+    messages: 'Classifica Messaggi',
+    voice: 'Classifica Canali Vocali',
+    invites: 'Classifica Inviti',
+    none: 'nessuno',
+    personalized: 'La tua Posizione',
+    total: '{amount} ;[gli utenti sono, l\'utente è] su questa classifica'
+};
