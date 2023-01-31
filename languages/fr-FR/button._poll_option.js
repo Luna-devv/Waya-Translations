7a6914c35276c03f68e86b7a88bf49d8a1a824c3
Luna <luni.siaa@gmail.com>
2023-01-31 08:31:26 +0100

    New translations button._poll_option.js (French)

---
+++ languages/fr-FR/button._poll_option.js
@@ -0,0 +1,7 @@
+module.exports = {
+    noPoll: 'Ce sondage est introuvable !',
+    ended: 'Ce sondage s\'est terminé il y a {time}.',
+    alreadyVoted: 'Vous avez déjà voté pour **#{num}**.',
+    voted: 'Vous avez voté **#{num}** à ce sondage.',
+    end: '**{users} ;[utilisateur, utilisateurs]** ;[a, ont] voté pour **{options}** durant les dernières {time} !'
+};    // syntax: ;[singular, plural]
