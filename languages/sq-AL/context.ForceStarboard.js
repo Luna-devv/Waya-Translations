339c24f39a93789f36ec82c633bc5c0d09310063
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:28 +0100

    New translations context.ForceStarboard.js (Albanian)

---
+++ languages/sq-AL/context.ForceStarboard.js
@@ -0,0 +1,12 @@
+module.exports = {
+    fail: {
+        config: 'Starboard nuk është konfiguruar për këtë Server..',
+        content: 'Mesazhi nuk ka përmbajtje as bashkëngjitje..',
+        channel: 'Kanali i konfiguruar Starboard nuk ekziston më..',
+        exists: {
+            text: 'Mesazhi është tashmë në Starboard..',
+            button: 'Mesazh/i'
+        }
+    },
+    success: 'Mesazhi është tani në Starboard.'
+};
