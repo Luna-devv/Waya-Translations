e976d217ad0b0e143e3382f9735ee3f0cb49fb8d
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:59 +0100

    New translations interaction.leaderboard.js (UwU English)

---
+++ languages/en-UWU/interaction.leaderboard.js
@@ -0,0 +1,8 @@
+module.exports = {
+    messages: 'Message Weadewboawd',
+    voice: 'Voice Weadewboawd',
+    invites: 'Invite Weadewboawd',
+    none: 'nyonye',
+    personalized: 'Ur Position',
+    total: '{amount} ;[usews r, usew is] on this weadewboawd'
+};
