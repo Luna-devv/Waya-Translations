c6bd60ff34ddf9ce88eff7429483e3cf79d19b0b
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:55 +0100

    New translations interaction.leaderboard.js (German)

---
+++ languages/de-DE/interaction.leaderboard.js
@@ -0,0 +1,8 @@
+module.exports = {
+    messages: 'Nachrichten Leaderboard',
+    voice: 'Voicechat Leaderboard',
+    invites: 'Einladungs Bestenliste',
+    none: 'keiner',
+    personalized: 'Deine Position',
+    total: '{amount} ;[users sind, user ist] auf diesem Leaderboard'
+};
