ea2a0ba2ac633c38780e48c7f1d2cf43eabe4961
Luna <luni.siaa@gmail.com>
2023-01-31 09:14:27 +0100

    Delete languages/pt directory

--- languages/pt/interaction.leaderboard.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    messages: 'Message Leaderboard',
-    voice: 'Voice Leaderboard',
-    invites: 'Invite Leaderboard',
-    none: 'none',
-    personalized: 'Your Position',
-    total: '{amount} ;[users are, user is] on this leaderboard'
-};
