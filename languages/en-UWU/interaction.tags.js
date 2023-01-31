c6e1317b3797b1d145ffddfb6f83075c9cefd03b
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:23 +0100

    New translations interaction.tags.js (UwU English)

---
+++ languages/en-UWU/interaction.tags.js
@@ -0,0 +1,95 @@
+module.exports = {
+    lengthFail: 'OH nyoo babe, the Tag nyame cannyot be wongew than {max} chawactews! >~<',
+    alreadyExists: 'A Tag with this nyame awweady exists!',
+    tagsLimit: 'U have weached the guiwd wimit of **{count}/{max} tags**!',
+    regexFail: 'Tag nyame vawidation faiwed fow using SwashCommands! ://///\nNyote that speciaw chawactews r nyot awwowed. 😔',
+    alreadyExistsDefault: 'A defauwt Command with this nyame awweady exists!',
+    invalidTag: 'A Tag with this nyame doesn\'t exist!',
+    info: {
+        title: {
+            created: 'Cweated',
+            author: 'Authoww',
+            usesWeek: 'Uses a Week',
+            uses: 'Uses',
+            last: 'Wast Used',
+            name: 'Tag Nyame',
+            slash: 'Swash Command',
+            channels: 'Channyews',
+            tags: 'Tags',
+            totalUses: 'Totaw Uses',
+            totalTags: 'Totaw Tags',
+            totalChannels: 'Totaw Channyews'
+        },
+        description: {
+            usesWeek: '{number}/wk',
+            // wk: week;; example: 43/wk
+            slash: {
+                true: 'active',
+                false: 'nyot active'
+            },
+            channelUses: '{number} ;[use, uses]'
+        },
+        noData: 'nyo data'
+    },
+    overview: {
+        title: 'Tag Config',
+        message: 'Message',
+        embed: 'Embed',
+        placeholder: 'Useabwe Pwacehowdews',
+        button: {
+            exit: 'Save & Exit',
+            permission: 'Sewect a pewmission the usew nyeeds',
+            color: {
+                enabled: 'Sewect a cowow fow the Embed',
+                disabled: 'Use Embeds and sewect a cowow'
+            },
+            group: {
+                ticket: {
+                    disable: 'Disabwe Ticket Tag',
+                    enable: 'Enyabwe Ticket Tag'
+                }
+            },
+            aliases: 'Manyage Nyame Awiases',
+            permissions: {
+                Administrator: 'Adminyistwatow',
+                ManageChannels: 'Manyage Channyews',
+                ManageGuild: 'Manyage Guiwd',
+                ManageMessages: 'Manyage Messages',
+                ManageNicknames: 'Manyage Nyicknyames',
+                ManageRoles: 'Manyage Wowes',
+                ManageWebhooks: 'Manyage Webhooks',
+                ManageEmojisAndStickers: 'Manyage Emojis & Stickews',
+                ManageEvents: 'Manyage Events',
+                ManageThreads: 'Manyage Thweads',
+                none: 'Nyonye'
+            }
+        }
+    },
+    form: {
+        formName: '{name} tag',
+        message: { content: 'Content' },
+        embed: {
+            title: 'Titwe',
+            description: 'Descwiption',
+            thumbnail: 'Thumbnyaiw',
+            image: 'Image',
+            footer: 'Footew'
+        },
+        aliases: { name: 'Nyame Awiases ({max} max)' }
+    },
+    success: {
+        message: 'Successfuwwy added the Tag {name}.',
+        command: 'Twy it out with {command}'
+    },
+    list: {
+        title: {
+            text: 'Text ;[Command, Commands]',
+            slash: 'Swash ;[Command, Commands]'
+        },
+        none: 'nyo tags',
+        disabled: '{amount} ;[tag is, tags r] hidden cuz u r missing pewmissions',
+        // ;[singular, plural]
+        amount: '{amount} tags • Use w/ {command}'
+    },
+    deleted: 'Successfuwwy uwu deweted the Tag **{nyame}**. >.<'
+};
