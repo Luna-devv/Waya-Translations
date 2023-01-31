b62fe4ce3d5995f54acd45e419814b998ac034dc
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:24 +0100

    New translations module.formatTime.js (French)

---
+++ languages/fr-FR/module.formatTime.js
@@ -0,0 +1,8 @@
+// ;[singular, plural]
+module.exports = {
+    months: ';[mois, mois]',
+    days: ';[jour, jours]',
+    hours: ';[heure, heures]',
+    minutes: ';[minute, minutes]',
+    seconds: ';[seconde, secondes]'
+};
