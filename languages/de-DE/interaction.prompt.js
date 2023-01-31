61781155cbec4edb786026505e3f05ad2f00dcc3
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:00 +0100

    New translations interaction.prompt.js (German)

---
+++ languages/de-DE/interaction.prompt.js
@@ -0,0 +1,29 @@
+module.exports = {
+    validate: 'Validiere Daten...',
+    create: 'Erstelle {type} Prompt...',
+    tooManyRoles: 'Du hast {roles} rollen auf deinem Server, Bitte stelle sicher, dass du weniger als 244 hast!',
+    created: 'Die {type} Prompt wurde erstellt.',
+    type: {
+        category: 'kategorie',
+        support: 'support rolle',
+        emote: 'Benutzerdefiniertes Emoji',
+        endPoll: 'abstimmung enden'
+    },
+    message: {
+        created: 'erstellt',
+        noSupport: 'keine (benutze **/config**)',
+        invalidEmote: 'nicht im selben Server',
+        content: 'Inhalt',
+        endPoll: 'benutzte {command}'
+    },
+    error: {
+        tooLong: 'Option {index} hat mehr als {number} Buchstaben!',
+        noData: 'keine daten angegeben'
+    },
+    verifySetUp: '**CAPTCHA Setup ist erforderlich**\nBitte benutze {command} und gehe zu *\'Logging > Joins > CAPTCHA\'* und führe diesen Befehl erneut.',
+    webedit: {
+        title: 'Nachricht Web Editor',
+        description: 'Öffne [diese Website]({url}), um die Nachricht zu bearbeiten und einzubetten.',
+        open: 'Web Editor öffnen'
+    }
+};
