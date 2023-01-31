31a05db422197a5c411a7d060cad3263171874e5
Luna <luni.siaa@gmail.com>
2023-01-31 08:34:10 +0100

    New translations module.verification.js (Italian)

---
+++ languages/it-IT/module.verification.js
@@ -0,0 +1,21 @@
+module.exports = {
+    join: {
+        title: 'Benvenuto/a {user}!',
+        description: 'Per accedere a questo Server, dovrai completare una rapida verifica per ridurre il rischio di attacchi ai bot e per garantire la sicurezza di revisione {emote}\n\nNota che gli amministratori e i Moderatori di {guild} non hanno accesso a nessuna delle tue informazioni.',
+        premium: 'Grazie per essere un membro Abbonato e per aver supportato questo progetto {emote}\n\nNota che non è necessario passare la verifica in {guild} poiché la verifica saltata fa parte del tuo piano Abbonamento.'
+    },
+    punishment: {
+        ban: 'Bannato da {guild}',
+        kick: 'Espulso da {guild}',
+        timeout: 'Timeout in {guild}',
+        role: 'Ruolo assegnato in {guild}'
+    },
+    reason: {
+        filter: 'L\'account è più giovane di {days} giorni ({relative})',
+        captcha: 'Verifica Fallita (CAPTCHA)'
+    },
+    button: {
+        verify: 'Verifica Completata',
+        privacy: 'Politica sulla Privacy'
+    }
+};
