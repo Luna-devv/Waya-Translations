90a64077da0c8e417c63cdf2282d63bf541a9db8
Luna <luni.siaa@gmail.com>
2023-01-31 08:34:03 +0100

    New translations command.snipe.js (UwU English)

---
+++ languages/en-UWU/command.snipe.js
@@ -0,0 +1,9 @@
+module.exports = {
+    none: 'Thewe wewe nyo Deweted/Edited messages in the wast {time} :(',
+    header: 'Fwom {user} in {channel}:',
+    disclaimer: 'The watest {amount} snyipes in the wast {time} qwq',
+    type: {
+        edit: 'Edit:',
+        delete: 'Dewete:'
+    }
+};
