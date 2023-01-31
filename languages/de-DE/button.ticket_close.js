b51f25d7bd73302bc227af288b074eb0087e7b1c
Luna <luni.siaa@gmail.com>
2023-01-31 08:31:46 +0100

    New translations button.ticket_close.js (German)

---
+++ languages/de-DE/button.ticket_close.js
@@ -0,0 +1,17 @@
+module.exports = {
+    request: { ureStaff: 'Du bist ein Staff, Du kannst einfach {command} verwenden.' },
+    noData: 'Der Server kann nicht in der Datenbank gefunden werden!',
+    confirm: {
+        text: 'Bist du dir sicher, dass du {channel} für immer löschen willst?',
+        configReason: 'Um einen Schließungsgrund anzugeben, aktiviere bitte Logging in `/config` => __Tickets__!',
+        required: 'Ein Schließungsgrund muss angegeben werden!',
+        button: {
+            close: 'Ticket schließen',
+            closeReason: 'Ticket mit Grund schließen'
+        }
+    },
+    form: {
+        formTitle: `Schließe {channel}`,
+        reason: 'Grund'
+    }
+};
