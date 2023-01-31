c14c8d34b8b1a2230221225c76a26a67c35c38a6
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:43 +0100

    New translations command.premium.js (French)

---
+++ languages/fr-FR/command.premium.js
@@ -0,0 +1,11 @@
+module.exports = {
+    set: {
+        noGuild: 'Veuillez entrer l\'ID du serveur sur lequel vous voulez activer votre abonnement Entreprise. [Comment obtenir l\'ID ?]({article})',
+        noSubscription: 'Vous n\'avez pas souscrit à un abonnement Entreprise. Si vous voulez accéder à des avantages spéciaux et que vous voulez soutenir le développement, pensez à [vous abonner à Entreprise]({url}).',
+        onCooldown: 'Vous utilisez cette commande trop souvent, vous pouvez changer de serveur tous les {days} jours, {remaining}.',
+        notFound: 'Ce serveur est introuvable, veuillez vérifier que je suis bien dans ce serveur !',
+        noPerms: '{guild} n\'est pas votre propriété, ou vous n\'avez pas la permission de le gérer !',
+        alreadyHas: '{guild} semble déjà avoir un abonnement Entreprise appliqué !',
+        success: 'Youhou, {user} ! Vous avez activé Entreprise sur {guild} !'
+    }
+};
