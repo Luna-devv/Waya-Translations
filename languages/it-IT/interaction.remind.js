ad8ec52f3496359cac325e8e854a326e89d82288
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:06 +0100

    New translations interaction.remind.js (Italian)

---
+++ languages/it-IT/interaction.remind.js
@@ -0,0 +1,24 @@
+module.exports = {
+    error: {
+        tooShort: 'La durata non può essere meno di **1 secondo**..',
+        tooLong: 'La durata non può essere più di **24 giorni**..',
+        cantFindId: 'Non riesco a trovare alcun promemoria attivo con l\'ID `{id}`'
+    },
+    reminder: 'Ehi {user}, ecco il tuo promemoria.',
+    remindSet: {
+        created: 'Ti ricorderò in **{time}**!',
+        id: 'Id'
+    },
+    remindView: {
+        created: 'Creato',
+        remindsIn: 'Promemoria',
+        message: 'Messaggio',
+        button: {
+            delete: 'Elimina',
+            invite: 'Invita {user}'
+        },
+        reminderCount: 'Hai {count} promemoria',
+        createFirst: 'Usa {command} prima per creare un promemoria!'
+    },
+    remindDelete: { deleted: 'Ho eliminato il tuo promemoria con l\'Id `{id}`' }
+};
