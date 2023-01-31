e40aed31fd5fe56de001e5198b717a142324d242
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:21 +0100

    New translations command.votereminder.js (Italian)

---
+++ languages/it-IT/command.votereminder.js
@@ -0,0 +1,8 @@
+module.exports = {
+    fail: 'È necessario fornire `true` o `false` come argomento.',
+    success: {
+        disable: 'Hai disabilitato il promemoria del voto.',
+        enable: 'Hai abilitato il promemoria di voto.'
+    },
+    button: { vote: 'Vota su Top.gg' }
+};
