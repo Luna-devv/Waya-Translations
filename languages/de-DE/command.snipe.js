7ab3fa464e57dcb5584a52847bf2422b3420cbbf
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:59 +0100

    New translations command.snipe.js (German)

---
+++ languages/de-DE/command.snipe.js
@@ -0,0 +1,9 @@
+module.exports = {
+    none: 'Es gibt keine gelöschten/bearbeiteten Nachrichten in den letzten {time}.',
+    header: 'Von {user} in {channel}',
+    disclaimer: 'Die neuesten {amount} Schnipes in den letzten {time}',
+    type: {
+        edit: 'Bearbeitet:',
+        delete: 'Gelöscht:'
+    }
+};
