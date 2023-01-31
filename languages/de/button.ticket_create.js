3701b54ed93c7b87e91b703cd09bf7142eebecf8
Luna <luni.siaa@gmail.com>
2023-01-31 08:26:50 +0100

    New translations button.ticket_create.js (German)

--- languages/de/button.ticket_create.js
+++ languages/de/button.ticket_create.js
@@ -1,15 +1,15 @@
 module.exports = {
-    noData: 'Could not find guild in the Database!',
-    noCategory: 'The Tickets category has been deleted. Please notify a Server Administrator.',
-    created: 'Your ticket {channel} has been created!',
+    noData: 'Der Server kann nicht in der Datenbank gefunden werden!',
+    noCategory: 'Die Ticketkategorie wurde gelöscht, bitte kontaktiere einen Server Administrator.',
+    created: 'Dein Ticket, {channel}, wurde erstellt!',
     form: {
-        formTitle: 'Ticket Opening Reason',
-        field: 'Why do you want to open a Ticket?',
-        placeholder: 'I need help with that..'
+        formTitle: 'Ticket Öffnungsgrund',
+        field: 'Warum willst du ein Ticket öffnen?',
+        placeholder: 'Ich brauche Hilfe mit..'
     },
-    commands: { closerequest: 'Create a close request' },
+    commands: { closerequest: 'Erstelle eine Schließungsanfrage' },
     max: {
-        category: 'You cannot create a Ticket at this time as this server reached [the max channel limit per category]({docs}) of {num}. Please tell the server admins to set the category channel to {string} to resolve this issue!\n\nHaving questions? {support}',
-        guild: 'You cannot create a Ticket at this time as this server reached the max channel limit of {num}. Please tell the server admins to delete a few channels to resovle this issue!\n\nHaving questions? {support}'
+        category: 'Du kannst derzeit kein Ticket erstellen, da dieser Server [das maximale Kanallimit pro Kategorie]({docs}) von {num} erreicht hat. Bitte teile den Serveradministratoren mit, den Kategoriekanal auf {string} zu setzen, um dieses Problem zu lösen!\n\nHast noch Fragen? {support}',
+        guild: 'Du kannst derzeit kein Ticket erstellen, da dieser Server das maximale Kanallimit von {num} erreicht hat. Bitte sage den Serveradministratoren, ein paar Kanäle zu löschen, um dieses Problem neu zu beheben!\n\nHast noch Fragen? {support}'
     }
 };
