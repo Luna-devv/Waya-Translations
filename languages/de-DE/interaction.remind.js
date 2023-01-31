5612c5ea1eccfa832a792633c19a68e33693c603
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:05 +0100

    New translations interaction.remind.js (German)

---
+++ languages/de-DE/interaction.remind.js
@@ -0,0 +1,24 @@
+module.exports = {
+    error: {
+        tooShort: 'Zeit kann nicht kürzer als **eine Sekunde** sein..',
+        tooLong: 'Zeit kann nicht mehr als **24 Tage** sein..',
+        cantFindId: 'Ich kann keine aktive Erinnerung mit der Id `{id}` finden'
+    },
+    reminder: 'Hey, {user}, hier ist deine Erinnerung.',
+    remindSet: {
+        created: 'Ich werde dich in **{time}** erinnern!',
+        id: 'Id'
+    },
+    remindView: {
+        created: 'Erstellt',
+        remindsIn: 'Erinnert',
+        message: 'Nachricht',
+        button: {
+            delete: 'Löschen',
+            invite: '{user} Einladen'
+        },
+        reminderCount: 'Du hast {count} Erinnerungen',
+        createFirst: 'Benutze zuerst {command}, um eine Erinnerung zu erstellen!'
+    },
+    remindDelete: { deleted: 'Ich habe deine Erinnerung mit der Id `{id}` gelöscht.' }
+};
