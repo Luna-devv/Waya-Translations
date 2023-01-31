9375a7f73a0d845f4df071dc04aeaaf1bbe3de25
Luna <luni.siaa@gmail.com>
2023-01-31 09:12:19 +0100

    Delete languages/de directory

--- languages/de/button._poll_option.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    noPoll: 'This poll could not be found!',
-    ended: 'This poll already ended {time} ago.',
-    alreadyVoted: 'You\'ve already voted for option **#{num}**.',
-    voted: 'You\'ve successfully voted for **#{num}** in this poll.',
-    end: '**{users} ;[user, users]** ;[has, have] voted for **{options} options** in the last {time}!'
-};    // syntax: ;[singular, plural]
