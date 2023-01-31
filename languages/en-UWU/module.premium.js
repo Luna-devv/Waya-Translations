349f10f3a5fa58c0b3344aef06de82462febd34e
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:53 +0100

    New translations module.premium.js (UwU English)

---
+++ languages/en-UWU/module.premium.js
@@ -0,0 +1,22 @@
+module.exports = {
+    end: {
+        title: 'Subscwiption End',
+        description: 'Hey {user}~ we\'ve just weceived ur subscwiption cancewation :(\nWe\'we sad to see u go, but we hope that u enjoyed it! {emote}',
+        notes: {
+            title: 'Pwease Nyote:',
+            support: 'If u nyeed hewp, pwese [Contact Suppowt]({support})',
+            reSubscribe: 'If u want to weSubscwibe, go to {url}'
+        }
+    },
+    new: {
+        title: 'Nyew Entewpwise Subscwiption',
+        description: 'Hey {user}~ we have just weceived ur successfuww payment. {emote}\nWe r weawwy happy to see u on boawd, and we hope that u wiww wike using the bot with ur nyew benyefits!',
+        notes: {
+            title: 'Pwease Nyote:',
+            setGuild: 'To set a Entewpwise guiwd, pwease use {command}',
+            getGuild: 'If u nyeed hewp with [getting ur Sewvew\'s Id]({article})',
+            cooldown: 'Don\'t fowget that u awways have a {days} days coowdown!',
+            support: 'If u nyeed hewp, pwese [Contact Suppowt]({support})'
+        }
+    }
+};
