baa8e7a9663c2b11e3f5469a870ab5f73569c32f
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:04 +0100

    New translations interaction.prompt.js (UwU English)

---
+++ languages/en-UWU/interaction.prompt.js
@@ -0,0 +1,29 @@
+module.exports = {
+    validate: 'Vawidate data...',
+    create: 'Cweating {type} pwompt...',
+    tooManyRoles: 'Girl, U have {roles} wowes in ur sewvew~!!1, pwease make suwe that u have wess than 244!1',
+    created: 'The {type} pwompt has been cweated.',
+    type: {
+        category: 'channyew categowy',
+        support: 'suppowt wowe',
+        emote: 'custom emote',
+        endPoll: 'end poww'
+    },
+    message: {
+        created: 'cweated',
+        noSupport: 'nyonye (use **/config**)',
+        invalidEmote: 'nyot in same guiwd',
+        content: 'Content',
+        endPoll: 'use {command}'
+    },
+    error: {
+        tooLong: 'Option {index} has mowe than {number} chawactews! meow~',
+        noData: 'nyo data set'
+    },
+    verifySetUp: '**CAPTCHA Setup is Wequiwed**\nPwease use {command} and go to *\'wogging > Joins > CAPTCHA\'* and wun this command again qwq',
+    webedit: {
+        title: 'Message Weeb~ Edit\n',
+        description: 'Open [this Weebsite]({uww}) to stawt editing the message & embed :;3',
+        open: 'Open Weeeb Edit'
+    }
+};
