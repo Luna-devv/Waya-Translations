18300378f333021bcbc806a8286c6bf42fa56ec4
Luna <luni.siaa@gmail.com>
2023-01-31 08:32:25 +0100

    New translations context.ForceStarboard.js (French)

---
+++ languages/fr-FR/context.ForceStarboard.js
@@ -0,0 +1,12 @@
+module.exports = {
+    fail: {
+        config: 'Le Starboard n\'est pas configuré dans ce serveur..',
+        content: 'Ce message n\'a pas ni contenu, ni pièce jointe..',
+        channel: 'Le salon du Starboard n\'existe plus..',
+        exists: {
+            text: 'Ce message est déjà dans le Starboard..',
+            button: 'Message'
+        }
+    },
+    success: 'Ce message est désormais dans le Starboard.'
+};
