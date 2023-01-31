d7c9c366c82fcc75cfa4afa0e0c5216f4633f358
Luna <luni.siaa@gmail.com>
2023-01-31 08:33:52 +0100

    New translations module.premium.js (Croatian)

---
+++ languages/hr-HR/module.premium.js
@@ -0,0 +1,22 @@
+module.exports = {
+    end: {
+        title: 'Kraj pretplate',
+        description: 'Hej {user}, upravo smo primili tvoje otkazivanje pretplate.\nŽao nam je što idete, ali se nadamo da ste uživali! {emote}',
+        notes: {
+            title: 'Imajte na umu:',
+            support: 'Ako trebate pomoć, molimo [Kontaktirajte podršku]({support})',
+            reSubscribe: 'Ako se želite ponovno pretplatiti, idite na {url}'
+        }
+    },
+    new: {
+        title: 'Nova Enterprise Pretplata',
+        description: 'Hej {user}, upravo smo primili vašu uspješnu uplatu. {emote}\nZaista smo sretni što vas vidimo sa nama i nadamo se da će vam se svidjeti korištenje bota s novim pogodnostima!',
+        notes: {
+            title: 'Imajte na umu:',
+            setGuild: 'Da biste postavili Enterprise server, koristite {command}',
+            getGuild: 'Ako trebate pomoć s [dobivanjem Id-a vašeg servera]({article})',
+            cooldown: 'Ne zaboravite da uvijek imate {days} dana hlađenja!',
+            support: 'Ako trebate pomoć, molimo [Kontaktirajte podršku]({support})'
+        }
+    }
+};
