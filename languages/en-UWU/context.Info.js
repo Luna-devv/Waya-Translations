907e0924b3473addf019ff736ab5d0168101dc15
Luna <luni.siaa@gmail.com>
2023-03-07 11:13:05 -0800

    New translations context.Info.js (UwU English)

--- languages/en-UWU/context.Info.js
+++ languages/en-UWU/context.Info.js
@@ -39,9 +39,10 @@ module.exports = {
         }
     },
     likes: {
-        add: 'Like',
-        remove: 'Liked',
-        added: 'You have liked {user}\'s profile. They now have {num} ;[like, likes] :3',
-        removed: 'You no longer like {user}\'s profile.'
+        add: 'Wike',
+        remove: 'Diswike',
+        added: 'U have wiked {usew}\'s pwofiwe. They nyow have {nyum} ;[wike, wikes] :3',
+        removed: 'U nyo wongew wike {usew}\'s pwofiwe. :(',
+        dm: '{emote} Congwatuwations, {name}!\n\nWe have some fantastic nyews to shr with u! ur {bot} pwofiwe has weached an incwedibwe {num} wikes! u\'we cweawwy doing something wight because peopwe just can\'t get enyough of u! :)))\n\nSo wet\'s keep the momentum going and spwead the wuv! Teww evewyonye to invite {bot} by sending them {link} and make suwe to use the {command} fowwowed by ur usewnyame to heawt 💖 ur pwofiwe!!!111\n\nThank u fow being such a fantastic pawt of ouw communyity! uwu\n\nCheews,\n{bot}-chan'
     }
 };
