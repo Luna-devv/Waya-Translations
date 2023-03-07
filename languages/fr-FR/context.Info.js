d9f7d1c4d1e55d9a9168a0a626b24972601d0ba5
Luna <luni.siaa@gmail.com>
2023-03-07 11:13:00 -0800

    New translations context.Info.js (French)

--- languages/fr-FR/context.Info.js
+++ languages/fr-FR/context.Info.js
@@ -40,8 +40,9 @@ module.exports = {
     },
     likes: {
         add: 'Like',
-        remove: 'Liked',
+        remove: 'Dislike',
         added: 'You have liked {user}\'s profile. They now have {num} ;[like, likes] :3',
-        removed: 'You no longer like {user}\'s profile.'
+        removed: 'You no longer like {user}\'s profile.',
+        dm: '{emote} Congratulations, {name}!\n\nWe have some fantastic news to share with you! Your {bot} profile has reached an incredible {num} likes! You\'re clearly doing something right because people just can\'t get enough of you!\n\nSo let\'s keep the momentum going and spread the love! Tell everyone to invite {bot} by sending them {link} and make sure to use the {command} followed by your username to heart 💖 your profile!\n\nThank you for being such a fantastic part of our community!\n\nCheers,\n{bot} '
     }
 };
