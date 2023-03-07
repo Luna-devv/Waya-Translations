74d8eeadb722dbab4de437b0570894b505c2279c
Luna <luni.siaa@gmail.com>
2023-03-07 12:18:44 -0800

    New translations interaction.debug.js (UwU English)

---
+++ languages/en-UWU/interaction.debug.js
@@ -0,0 +1,15 @@
+module.exports = {
+    state: {
+        operational: 'Opewationyaw',
+        issues: 'Le Problem?',
+        outage: 'Dead'
+    },
+    shards: 'Aww Shawds',
+    shard: 'This Shawd',
+    uptime: 'Shawd Uptime',
+    database: 'Book',
+    imageApi: 'Image Makew',
+    bot: 'Me',
+    milliseconds: 'ms',
+    page: 'See mowe bwoken times'
+};
