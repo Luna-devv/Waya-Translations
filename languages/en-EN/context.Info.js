73405053ead79f00b928941093115a2775d37932
Lunish <luni.siaa@gmail.com>
2023-03-07 20:13:10 +0100

    a

--- languages/en-EN/context.Info.js
+++ languages/en-EN/context.Info.js
@@ -37,5 +37,14 @@ module.exports = {
             kick: 'Successfully kicked {user} from {guild}.',
             timeout: 'Successfully timed {user} out in {guild}.',
         }
+    },
+    likes: {
+        add: 'Like',
+        remove: 'Dislike',
+
+        added: 'You have liked {user}\'s profile. They now have {num} ;[like, likes] :3',
+        removed: 'You no longer like {user}\'s profile.',
+
+        dm: '{emote} Congratulations, {name}!\n\nWe have some fantastic news to share with you! Your {bot} profile has reached an incredible {num} likes! You\'re clearly doing something right because people just can\'t get enough of you!\n\nSo let\'s keep the momentum going and spread the love! Tell everyone to invite {bot} by sending them {link} and make sure to use the {command} followed by your username to heart 💖 your profile!\n\nThank you for being such a fantastic part of our community!\n\nCheers,\n{bot} '
     }
 };
