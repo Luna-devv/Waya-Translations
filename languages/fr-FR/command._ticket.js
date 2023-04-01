616fa32862e653353755dd28db26bc1252fd3f0e
Luna <luni.siaa@gmail.com>
2023-04-01 13:19:57 +0200

    New translations command._ticket.js (French)

--- languages/fr-FR/command._ticket.js
+++ languages/fr-FR/command._ticket.js
@@ -1,5 +1,5 @@
 module.exports = {
-    noMember: 'Please provide a valid member as argument.',
+    noMember: 'Veuillez fournir un membre valide en argument.',
     add: { success: '{user} has been successfully added to this Ticket!' },
     remove: { success: '{user} has been successfully removed from this Ticket!' },
     claim: {
