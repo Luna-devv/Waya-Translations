cbda86b05c33781ae46054e95828e2d942d3c534
Luna <luni.siaa@gmail.com>
2023-04-11 16:19:55 -0700

    New translations module.verification.js (Polish)

--- languages/pl-PL/module.verification.js
+++ languages/pl-PL/module.verification.js
@@ -1,21 +1,21 @@
 module.exports = {
     join: {
-        title: 'Welcome {user}!',
-        description: 'In order to get access to this Server, you will have to complete a quick verification to lower the risk of Bot attacks and to ensure overhaul security {emote}\n\nPlease note the Admins and Mods of {guild} do not have access to any of your provided information.',
-        premium: 'Thank you for being an Enterprise member and supporting this project {emote}\n\nNote that you do not need to pass the verification in {guild} as skipping verification is part of your Enterprise plan.'
+        title: 'Witaj {user}!',
+        description: 'Aby uzyskać dostęp do tego serwera, musisz ukończyć szybką weryfikację, aby zmniejszyć ryzyko ataków botów i zapewnić większe bezpieczeństwo {emote}\n\nPamiętaj, że Administratorzy i Moderatorzy {guild} nie mają dostępu do podanych informacji.',
+        premium: 'Dziękujemy za bycie członkiem Enterprise i wsparcie tego projektu {emote}\n\nPamiętaj, że nie musisz przechodzić weryfikacji w {guild}, ponieważ pominięcie weryfikacji jest częścią planu Enterprise.'
     },
     punishment: {
-        ban: 'Banned from {guild}',
-        kick: 'Kicked from {guild}',
-        timeout: 'Timed out in {guild}',
-        role: 'Assigned Role in {guild}'
+        ban: 'Zbanowano z {guild}',
+        kick: 'Wyrzucono z {guild}',
+        timeout: 'Ztimeout\'owano z {guild}',
+        role: 'Przypisana Rola w {guild}'
     },
     reason: {
-        filter: 'Account is younger than {days} days ({relative})',
-        captcha: 'Failed Verification (CAPTCHA)'
+        filter: 'Konto jest młodsze niż {days} dni ({relative})',
+        captcha: 'Nieudana Weryfikacja (CAPTCHA)'
     },
     button: {
-        verify: 'Complete Verification',
-        privacy: 'Privacy Policy'
+        verify: 'Zakończ Weryfikację',
+        privacy: 'Polityka Prywatności'
     }
 };
