2a4701ad31b8104146ce39754cc0af26554d13bd
Luna <luni.siaa@gmail.com>
2023-04-11 14:18:43 -0700

    New translations command.tags.js (Polish)

--- languages/pl-PL/command.tags.js
+++ languages/pl-PL/command.tags.js
@@ -1,15 +1,15 @@
 module.exports = {
     title: {
-        text: 'Text ;[Command, Commands]',
-        slash: 'Slash ;[Command, Commands]'
+        text: 'Tekst ;[Komenda, Komendy]',
+        slash: 'Ukośnik ;[Komenda, Komendy]'
     },
-    none: 'no tags',
-    disabled: '{amount} ;[tag is, tags are] hidden due to missing permissions',
+    none: 'brak tagów',
+    disabled: '{amount} ;[tag jest ukryty, tagi są ukryte] z powodu braku uprawnień',
     // ;[singular, plural]
-    amount: '{amount} tags • Use with {command}',
+    amount: '{amount} tagów • Używaj z {command}',
     errors: {
-        overLimit: 'You have reached the guild limit of **{count}/{max} tags**!\n\nNote that this tag exceeds the limits and is disabled until you increase the limit again.',
-        invalidTag: 'A Tag with this name doesn\'t exist!',
-        ticketUse: 'This Tag is only available in tickets using {user}.'
+        overLimit: 'Osiągnąłeś limit gildii **{count}/{max} tagów**!\n\nZauważ, że ten tag przekracza limity i jest wyłączony do czasu zwiększenia limitu.',
+        invalidTag: 'Tag o tej nazwie nie istnieje!',
+        ticketUse: 'Ten tag jest dostępny tylko w zgłoszeniach używając {user}.'
     }
 };
