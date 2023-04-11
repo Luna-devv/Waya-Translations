af51a08af3b0cb7b77b5d0f37e9e32f60b96ff2e
Luna <luni.siaa@gmail.com>
2023-04-11 13:15:49 -0700

    New translations button.ticket_claim.js (Polish)

---
+++ languages/pl-PL/button.ticket_claim.js
@@ -0,0 +1,10 @@
+module.exports = {
+    noData: 'Nie udało się znaleźć tego serwera w bazie danych!',
+    claimed: 'Zgłoszenie będzie teraz obsługiwane przez Ciebie!',
+    commands: {
+        add: 'Dodaj użytkownika do tego zgłoszenia',
+        remove: 'Usuń użytkownika z tego zgłoszenia',
+        close: 'Zamknij zgłoszenie',
+        tags: 'Wyświetl wszystkie tagi'
+    }
+};
