8f93d15ad430455f71c012d0a62c13210a8d5441
Luna <luni.siaa@gmail.com>
2023-04-11 15:23:15 -0700

    New translations interaction.help.js (Polish)

--- languages/pl-PL/interaction.help.js
+++ languages/pl-PL/interaction.help.js
@@ -1,8 +1,8 @@
 module.exports = {
-    description: '**About {name}**\nI\'m developed by {devs} icons by [Danu](https://waya.one/go/danu_icons).\n{name} is made to be very simple in it\'s configuration process but at the same time there to provide the best customizability. Everything can be done in one simple {config} command.\n\n**Accessability**\nIf you want to change the language please use {language}.\nIf you want to help us translate, join us on {translateSite}.\n\nIf it gets to overwhelming for you simply join the [Support Server]({support}) and ask the community in the Support Channel for help.',
-    footer: 'Online for {time}',
+    description: '**O {name}**\nJestem stworzony przez {devs} ikony przez [Danu](https://waya.one/go/danu_icons).\n{name} jest bardzo prosta w konfiguracji procesu, ale jednocześnie zapewnia najlepsze dostosowanie dla użytkowników. Wszystko można zrobić za pomocą jednej prostej komendy {config}.\n\n**Dostępność**\nJeśli chcesz zmienić język, użyj {language}.\nJeśli chcesz pomóc nam w tłumaczeniu, dołącz do nas {translateSite}.\n\nJeśli jest to dla ciebie za skomplikowane, po prostu dołącz do [Serwera Pomocy]({support}) i poproś społeczność w kanale Wsparcia o pomoc.',
+    footer: 'Online {time}',
     button: {
-        learnmore: 'Learn More',
-        vote: 'Vote'
+        learnmore: 'Dowiedz się więcej',
+        vote: 'Zagłosuj'
     }
 };
