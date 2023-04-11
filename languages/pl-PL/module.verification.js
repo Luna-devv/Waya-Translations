9c232bb781bd50e4e0ff96332d75a7239e12fa5d
Luna <luni.siaa@gmail.com>
2023-04-11 13:16:11 -0700

    New translations module.verification.js (Polish)

---
+++ languages/pl-PL/module.verification.js
@@ -0,0 +1,21 @@
+module.exports = {
+    join: {
+        title: 'Welcome {user}!',
+        description: 'In order to get access to this Server, you will have to complete a quick verification to lower the risk of Bot attacks and to ensure overhaul security {emote}\n\nPlease note the Admins and Mods of {guild} do not have access to any of your provided information.',
+        premium: 'Thank you for being an Enterprise member and supporting this project {emote}\n\nNote that you do not need to pass the verification in {guild} as skipping verification is part of your Enterprise plan.'
+    },
+    punishment: {
+        ban: 'Banned from {guild}',
+        kick: 'Kicked from {guild}',
+        timeout: 'Timed out in {guild}',
+        role: 'Assigned Role in {guild}'
+    },
+    reason: {
+        filter: 'Account is younger than {days} days ({relative})',
+        captcha: 'Failed Verification (CAPTCHA)'
+    },
+    button: {
+        verify: 'Complete Verification',
+        privacy: 'Privacy Policy'
+    }
+};
