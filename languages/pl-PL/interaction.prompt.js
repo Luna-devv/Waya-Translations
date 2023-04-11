25a85f50fd88770a68dd52cea766c038269493f5
Luna <luni.siaa@gmail.com>
2023-04-11 13:16:05 -0700

    New translations interaction.prompt.js (Polish)

---
+++ languages/pl-PL/interaction.prompt.js
@@ -0,0 +1,29 @@
+module.exports = {
+    validate: 'Validate data...',
+    create: 'Creating {type} prompt...',
+    tooManyRoles: 'You have {roles} roles in your server, please make sure that you have less than 244!',
+    created: 'The {type} prompt has been created.',
+    type: {
+        category: 'channel category',
+        support: 'support role',
+        emote: 'custom emote',
+        endPoll: 'end poll'
+    },
+    message: {
+        created: 'created',
+        noSupport: 'none (use **/config**)',
+        invalidEmote: 'not in same guild',
+        content: 'Content',
+        endPoll: 'use {command}'
+    },
+    error: {
+        tooLong: 'Option {index} has more than {number} characters!',
+        noData: 'no data set'
+    },
+    verifySetUp: '**CAPTCHA Setup is Required**\nPlease use {command} and go to *\'Logging > Joins > CAPTCHA\'* and run this command again.',
+    webedit: {
+        title: 'Message Web Edit',
+        description: 'Open [this Website]({url}) to start editing the message & embed.',
+        open: 'Open Web Edit'
+    }
+};
