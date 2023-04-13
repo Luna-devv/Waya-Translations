cebdb66c785d1ac2d3cf284e4f240aa0a038f546
Luna <luni.siaa@gmail.com>
2023-04-13 09:05:50 -0700

    New translations interaction.pace.js (UwU English)

---
+++ languages/en-UWU/interaction.pace.js
@@ -0,0 +1,4 @@
+module.exports = {
+    noData: 'I have nyo data fow that usew. :(((',
+    content: 'Based on ;[ur, {user}\'s] awwtime avewage of {perday} messages pew day, my estimate is that ;[u, they] wiww weach {msgs} messages by {date}.'
+};
