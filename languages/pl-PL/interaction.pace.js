be088dc4d1300e605529e58b063e57d213aa1c95
Luna <luni.siaa@gmail.com>
2023-04-13 10:05:56 -0700

    New translations interaction.pace.js (Polish)

--- languages/pl-PL/interaction.pace.js
+++ languages/pl-PL/interaction.pace.js
@@ -1,4 +1,4 @@
 module.exports = {
-    noData: 'I have no data for that user.',
+    noData: 'Nie mam danych dla tego użytkownika.',
     content: 'Based on ;[your, {user}\'s] alltime average of {perday} messages per day, my estimate is that ;[you, they] will reach {msgs} messages by {date}.'
 };
