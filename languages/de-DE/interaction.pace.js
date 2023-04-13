d961748e617546dc47f88bdc9a4447ea54196b17
Luna <luni.siaa@gmail.com>
2023-04-13 09:05:28 -0700

    New translations interaction.pace.js (German)

---
+++ languages/de-DE/interaction.pace.js
@@ -0,0 +1,4 @@
+module.exports = {
+    noData: 'Ich habe keine Daten für diesen User.',
+    content: 'Basierend auf ;[deiner, {user}\'s] im avg von {perday} Nachrichten pro Tag ist meine Schätzung das ;[du, sie] {msgs} Nachrichten am {date} Erreichen wird.'
+};
