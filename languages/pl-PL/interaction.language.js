9ae2e653ac83343937b23bf1b3f5b4bae63d307e
Luna <luni.siaa@gmail.com>
2023-04-16 03:46:36 +0200

    New translations interaction.language.js (Polish)

--- languages/pl-PL/interaction.language.js
+++ languages/pl-PL/interaction.language.js
@@ -1,1 +1,1 @@
-module.exports = { updated: 'Twój podstawowy język został zaktualizowany do **{locale}**!' };
+module.exports = { updated: 'Twój podstawowy język został zaktualizowany na **{locale}**!' };
