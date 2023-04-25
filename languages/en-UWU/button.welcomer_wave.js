10bc2b1caa6a5cf046959baf772e44d067d6a867
Luna <luni.siaa@gmail.com>
2023-04-25 16:05:23 +0200

    New translations button.welcomer_wave.js (UwU English)

--- languages/en-UWU/button.welcomer_wave.js
+++ languages/en-UWU/button.welcomer_wave.js
@@ -1,4 +1,5 @@
 module.exports = {
+    limit: 'You can only greet members once to avoid spam :(',
     noHook: 'This featuwe has been disabwed by sewvew admins. :(',
     disclaimer: 'HEY! This message has been configuwed by sewvew admins. {bot} is committed to pwoviding a safe ;-;;; and wespectfuw enviwonment fow aww usews. We do nyot suppowt ow towewate any hawmfuw ow offensive >.< content wanguage. :))'
 };
