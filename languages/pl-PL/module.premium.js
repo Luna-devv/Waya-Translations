bc6e0face50dcf28162bcd010359994353bc3764
Luna <luni.siaa@gmail.com>
2023-06-04 17:05:09 +0200

    New translations module.premium.js (Polish)

--- languages/pl-PL/module.premium.js
+++ languages/pl-PL/module.premium.js
@@ -1,22 +1,11 @@
 module.exports = {
     end: {
-        title: 'Koniec subskrypcji',
-        description: 'Cześć {user}, właśnie otrzymaliśmy anulowanie subskrypcji.\nPrzykro nam, że rezygnujesz, ale mamy nadzieję, że Ci się podobało! {emote}',
-        notes: {
-            title: 'Uwaga:',
-            support: 'Jeśli potrzebujesz pomocy, [Skontaktuj się z działem wsparcia]({support})',
-            reSubscribe: 'Jeśli chcesz ponownie subskrybować, przejdź do {url}'
-        }
+        title: 'Subscription canceled',
+        description: 'We are sorry to see that your subscription with {bot} Enterprise has been cancelled. We appreciate your support during your time with us and hope that you have enjoyed the benefits of our service.\n\nIf there is anything we can do to improve your experience or if you have any feedback to share, please do not hesitate to contact us. Your opinion matters to us, and we are committed to continuously improving our service.\n\nBest regards,\n{dev}'
     },
     new: {
-        title: 'Nowa subskrypcja Enterprise',
-        description: 'Cześć {user}, otrzymaliśmy Twoją płatność. {emote}\nNaprawdę cieszymy się widząc cię na pokładzie, i mamy nadzieję, że polubisz używanie bota z nowymi korzyściami!',
-        notes: {
-            title: 'Uwaga:',
-            setGuild: 'Aby ustawić gildię Enterprise, użyj {command}',
-            getGuild: 'Jeśli potrzebujesz pomocy z [uzyskaniem ID Serwera]({article})',
-            cooldown: 'Nie zapomnij, że zawsze masz {days} dni odnowienia!',
-            support: 'Jeśli potrzebujesz pomocy, [Skontaktuj się z działem wsparcia]({support})'
-        }
-    }
+        title: 'Welcome to {bot} Enterprise',
+        description: 'Thank you so much for subscribing to {bot} Enterprise, {user}! Your support means the world to us, and we truly appreciate your trust in our services. ❤️\n\nWe are dedicated to providing you with the best possible experience and ensuring that your needs are met. If you ever have any questions or concerns, please don\'t hesitate to reach out to us.\n\nBest regards,\n{dev}'
+    },
+    position: 'Developer, Customer Service Representative'
 };
