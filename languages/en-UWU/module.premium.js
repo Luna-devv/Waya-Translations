6352f90dbc93356c24fece754b6e7ce90a66d3a7
Luna <luni.siaa@gmail.com>
2023-06-04 17:07:33 +0200

    New translations module.premium.js (UwU English)

--- languages/en-UWU/module.premium.js
+++ languages/en-UWU/module.premium.js
@@ -1,22 +1,11 @@
 module.exports = {
     end: {
-        title: 'Subscwiption End',
-        description: 'Hey {user}~ we\'ve just weceived ur subscwiption cancewation :(\nWe\'we sad to see u go, but we hope that u enjoyed it! {emote}',
-        notes: {
-            title: 'Pwease Nyote:',
-            support: 'If u nyeed hewp, pwese [Contact Suppowt]({support})',
-            reSubscribe: 'If u want to weSubscwibe, go to {url}'
-        }
+        title: 'Subscription canceled',
+        description: 'We are sorry to see that your subscription with {bot} Enterprise has been cancelled. We appreciate your support during your time with us and hope that you have enjoyed the benefits of our service.\n\nIf there is anything we can do to improve your experience or if you have any feedback to share, please do not hesitate to contact us. Your opinion matters to us, and we are committed to continuously improving our service.\n\nBest regards,\n{dev}'
     },
     new: {
-        title: 'Nyew Entewpwise Subscwiption',
-        description: 'Hey {user}~ we have just weceived ur successfuww payment. {emote}\nWe r weawwy happy to see u on boawd, and we hope that u wiww wike using the bot with ur nyew benyefits!',
-        notes: {
-            title: 'Pwease Nyote:',
-            setGuild: 'To set a Entewpwise guiwd, pwease use {command}',
-            getGuild: 'If u nyeed hewp with [getting ur Sewvew\'s Id]({article})',
-            cooldown: 'Don\'t fowget that u awways have a {days} days coowdown!',
-            support: 'If u nyeed hewp, pwese [Contact Suppowt]({support})'
-        }
-    }
+        title: 'Welcome to {bot} Enterprise',
+        description: 'Thank you so much for subscribing to {bot} Enterprise, {user}! Your support means the world to us, and we truly appreciate your trust in our services. ❤️\n\nWe are dedicated to providing you with the best possible experience and ensuring that your needs are met. If you ever have any questions or concerns, please don\'t hesitate to reach out to us.\n\nBest regards,\n{dev}'
+    },
+    position: 'Developer, Customer Service Representative'
 };
