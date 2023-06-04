06222064566fcd3d9a2644d8293a76f26c0a96bf
Luna <luni.siaa@gmail.com>
2023-06-04 17:06:50 +0200

    New translations module.premium.js (Croatian)

--- languages/hr-HR/module.premium.js
+++ languages/hr-HR/module.premium.js
@@ -1,22 +1,11 @@
 module.exports = {
     end: {
-        title: 'Kraj pretplate',
-        description: 'Hej {user}, upravo smo primili tvoje otkazivanje pretplate.\nŽao nam je što idete, ali se nadamo da ste uživali! {emote}',
-        notes: {
-            title: 'Imajte na umu:',
-            support: 'Ako trebate pomoć, molimo [Kontaktirajte podršku]({support})',
-            reSubscribe: 'Ako se želite ponovno pretplatiti, idite na {url}'
-        }
+        title: 'Subscription canceled',
+        description: 'We are sorry to see that your subscription with {bot} Enterprise has been cancelled. We appreciate your support during your time with us and hope that you have enjoyed the benefits of our service.\n\nIf there is anything we can do to improve your experience or if you have any feedback to share, please do not hesitate to contact us. Your opinion matters to us, and we are committed to continuously improving our service.\n\nBest regards,\n{dev}'
     },
     new: {
-        title: 'Nova Enterprise Pretplata',
-        description: 'Hej {user}, upravo smo primili vašu uspješnu uplatu. {emote}\nZaista smo sretni što vas vidimo sa nama i nadamo se da će vam se svidjeti korištenje bota s novim pogodnostima!',
-        notes: {
-            title: 'Imajte na umu:',
-            setGuild: 'Da biste postavili Enterprise server, koristite {command}',
-            getGuild: 'Ako trebate pomoć s [dobivanjem Id-a vašeg servera]({article})',
-            cooldown: 'Ne zaboravite da uvijek imate {days} dana hlađenja!',
-            support: 'Ako trebate pomoć, molimo [Kontaktirajte podršku]({support})'
-        }
-    }
+        title: 'Welcome to {bot} Enterprise',
+        description: 'Thank you so much for subscribing to {bot} Enterprise, {user}! Your support means the world to us, and we truly appreciate your trust in our services. ❤️\n\nWe are dedicated to providing you with the best possible experience and ensuring that your needs are met. If you ever have any questions or concerns, please don\'t hesitate to reach out to us.\n\nBest regards,\n{dev}'
+    },
+    position: 'Developer, Customer Service Representative'
 };
