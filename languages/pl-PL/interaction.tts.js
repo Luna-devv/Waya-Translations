979de525c3b7f1fe4d13ee2ea71949f629af43e2
Luna <luni.siaa@gmail.com>
2023-06-15 20:56:47 +0200

    New translations interaction.tts.js (Polish)

--- languages/pl-PL/interaction.tts.js
+++ languages/pl-PL/interaction.tts.js
@@ -1,13 +1,13 @@
 module.exports = {
     get: {
-        error: 'Something went wront when creating the voice.. Try a shorter text, might work then!',
-        vote: 'Please help us at {url}, only takes a few seconds',
-        add: '[Add {bot}]({url}) to your server'
+        error: 'Coś poszło nie tak podczas tworzenia głosu.. Spróbuj użyć krótszego tekstu, wtedy może zadziałać!',
+        vote: 'Pomóż nam w {url}, zajmie to tylko kilka sekund',
+        add: '[Dodaj {bot}]({url}) do swojego serwera'
     },
     set: {
-        title: 'Personal Text to Speech config',
-        voice: 'Default Voice: {voice}',
-        filetype: 'Default Type: {filetype}',
-        footer: 'The example audio respects all your personal options.'
+        title: 'Osobista konfiguracja tekstu na mowę',
+        voice: 'Głos domyślny: {voice}',
+        filetype: 'Domyślny typ pliku: {filetype}',
+        footer: 'Przykładowy dźwięk respektuje wszystkie Twoje osobiste opcje.'
     }
 };
