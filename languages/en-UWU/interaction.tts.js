166bc0f7745e298d4fd8487b7fcb840863ab9bed
Luna <luni.siaa@gmail.com>
2023-06-15 19:35:41 +0200

    New translations interaction.tts.js (UwU English)

---
+++ languages/en-UWU/interaction.tts.js
@@ -0,0 +1,13 @@
+module.exports = {
+    get: {
+        error: 'Something went wront when creating the voice.. Try a shorter text, might work then!',
+        vote: 'Please help us at {url}, only takes a few seconds',
+        add: '[Add {bot}]({url}) to your server'
+    },
+    set: {
+        title: 'Personal Text to Speech config',
+        voice: 'Default Voice: {voice}',
+        filetype: 'Default Type: {filetype}',
+        footer: 'The example audio respects all your personal options.'
+    }
+};
