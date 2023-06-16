c4c23847ff0e26288304d96d9d914577c7f2e8fe
Luna <luni.siaa@gmail.com>
2023-06-16 22:41:07 +0200

    New translations module.premium.js (German)

--- languages/de-DE/module.premium.js
+++ languages/de-DE/module.premium.js
@@ -1,11 +1,11 @@
 module.exports = {
     end: {
-        title: 'Subscription canceled',
-        description: 'We are sorry to see that your subscription with {bot} Enterprise has been cancelled. We appreciate your support during your time with us and hope that you have enjoyed the benefits of our service.\n\nIf there is anything we can do to improve your experience or if you have any feedback to share, please do not hesitate to contact us. Your opinion matters to us, and we are committed to continuously improving our service.\n\nBest regards,\n{dev}'
+        title: 'Abonnement beendet',
+        description: 'Es tut uns leid, dass dein Abonnement bei {bot} Enterprise gekündigt wurde. Wir danken dir über deine Unterstützung während dieser Zeit bei uns und hoffen, dass du die Vorteile unseres Dienstes genossen hast.\n\nWenn es etwas gibt, was wir tun können, um deine Erfahrung zu verbessern oder wenn du Feedback hast, das du teilen möchtest, zögere nicht uns zu kontaktieren. Deine Meinung ist uns wichtig und wir bemühen uns, unseren Service kontinuierlich zu verbessern.\n\nGrüße,\n{dev}'
     },
     new: {
-        title: 'Welcome to {bot} Enterprise',
-        description: 'Thank you so much for subscribing to {bot} Enterprise, {user}! Your support means the world to us, and we truly appreciate your trust in our services. ❤️\n\nWe are dedicated to providing you with the best possible experience and ensuring that your needs are met. If you ever have any questions or concerns, please don\'t hesitate to reach out to us.\n\nBest regards,\n{dev}'
+        title: 'Willkommen bei {bot} Enterprise',
+        description: 'Vielen Dank {user}, dass du {bot} Enterprise, abonniert hast! Deine Unterstützung bedeutet die Welt für uns, und wir schätzen dein Vertrauen in unsere Dienste. ❤️\n\nWir sind bestrebt, dir die bestmögliche Erfahrung zu geben und sicherzustellen, dass deine Bedürfnisse erfüllt werden. Wenn du jemals Fragen oder Bedenken hast, zögere bitte nicht uns zu kontaktieren.\n\nMit freundlichen Grüßen\n{dev}'
     },
-    position: 'Developer, Customer Service Representative'
+    position: 'Entwickler, Kundendienstvertreter'
 };
