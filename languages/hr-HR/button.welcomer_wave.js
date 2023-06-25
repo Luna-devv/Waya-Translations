9b287ab41b956bce589d3c7ca435cf0e206ede40
Luna <luni.siaa@gmail.com>
2023-06-25 15:45:51 +0200

    New translations button.welcomer_wave.js (Croatian)

--- languages/hr-HR/button.welcomer_wave.js
+++ languages/hr-HR/button.welcomer_wave.js
@@ -1,5 +1,5 @@
 module.exports = {
-    limit: 'You can only greet members once to avoid spam :(',
+    limit: 'Članove možete pozdraviti samo jednom kako biste izbjegli spam :(',
     noHook: 'Ovu značajku onemogućili su administratori servera.',
     disclaimer: 'Ovu poruku su konfigurirali administratori servera. {bot} je predan pružanju sigurnog okruženja punog poštovanja za sve korisnike. Ne podržavamo niti toleriramo štetan ili uvredljiv sadržaj ili jezik.'
 };
