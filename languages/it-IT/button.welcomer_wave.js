e2961870d24199b29ed61e2d96f41b935f9ed2de
Luna <luni.siaa@gmail.com>
2023-08-05 16:03:14 +0200

    New translations button.welcomer_wave.js (Italian)

--- languages/it-IT/button.welcomer_wave.js
+++ languages/it-IT/button.welcomer_wave.js
@@ -1,5 +1,5 @@
 module.exports = {
-    limit: 'You can only greet members once to avoid spam :(',
+    limit: 'Puoi salutare i membri solo una volta per evitare lo spam :(',
     noHook: 'Questa funzione è stata disabilitata dagli amministratori del server.',
     disclaimer: 'Questo messaggio è stato configurato dagli amministratori del server. {bot} si impegna a fornire un ambiente sicuro e rispettoso per tutti gli utenti. Non sosteniamo né tolleriamo alcun contenuto o linguaggio dannoso o offensivo.'
 };
